57bf032fe1d08a091056501810926a423741d06c
ajongs <wonjong1030@naver.com>
2023-02-01 21:47:32 +0900

    \#3 test(GasDetailTest): 주유소 csv 파일을 변환하는 GasDetail 정적 팩터리 메소드 성공 테스트코드 추가

--- backend/src/test/java/com/kaspi/backend/domain/GasDetailTest.java
+++ backend/src/test/java/com/kaspi/backend/domain/GasDetailTest.java
@@ -1,4 +1,30 @@
+package com.kaspi.backend.domain;
+
+import com.kaspi.backend.enums.GasType;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class GasDetailTest {
-  
+    @Test
+    @DisplayName("GasDetail 정적 팩터리 메소드 성공 테스트 (주유소)")
+    void parseGasDetail() {
+        String[] input = new String[]{"A0000035", "서울 종로구", "㈜지에스이앤알 평창주유소", "서울 종로구 평창문화로 135 (평창동)"
+                , "현대오일뱅크", "셀프", "1899", "1659", "1759", "0"};
+        System.out.println(input.length);
+        LocalDate date = LocalDate.now();
+        List<GasDetail> gasDetail = GasDetail.parseListGasDetail(input, date);
+
+        List<GasDetail> list = new ArrayList<>();
+        list.add(new GasDetail("A0000035", 1899, GasType.PREMIUM_GASOLINE, date));
+        list.add(new GasDetail("A0000035", 1659, GasType.GASOLINE, date));
+        list.add(new GasDetail("A0000035", 1759, GasType.DIESEL, date));
+        Assertions.assertThat(gasDetail).usingRecursiveComparison().isEqualTo(list);
+    }
 }
