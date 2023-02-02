540b0bae6e563d388e2d7e2ecffbd2fee82e058b
rentalhub-mac78 <kksy2423@naver.com>
2023-02-02 10:26:32 +0900

    #2 remove(backend): spring data jdbc 테스트파일 삭제

--- backend/src/main/java/com/kaspi/backend/test/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kaspi.backend.test;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestRepository extends CrudRepository<TestEntity, Long> {
-
-}
