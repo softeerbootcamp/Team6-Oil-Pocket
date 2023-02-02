a590b3cfe6b9905e94feac23de37f3bdc4cd1260
rentalhub-mac78 <kksy2423@naver.com>
2023-02-02 18:33:17 +0900

    #17 feat(dao): userdao를 활용한 insert, select 기능 추가

---
+++ backend/src/main/java/com/kaspi/backend/dao/UserDao.java
@@ -0,0 +1,23 @@
+package com.kaspi.backend.dao;
+
+import com.kaspi.backend.domain.User;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserDao extends CrudRepository<User, Long> {
+    @Query(value = "INSERT INTO users (id, password, gender, age) VALUES (:id, :password, :gender, :age)")
+    @Modifying
+    Long insertUser(@Param("id") String id, @Param("password") String password, @Param("gender") String gender, @Param("age") String age);
+
+
+    @Query("select * from users where user_no= :userNo")
+    Optional<User> findByUserNo(@Param("userNo") Long userNo);
+
+}
