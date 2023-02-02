cd7d131e1478125bae8eb11f12dd818497a8944e
rentalhub-mac78 <kksy2423@naver.com>
2023-02-02 18:32:22 +0900

    #17 test(dao): userdao를 활용한 insert, select 테스트

---
+++ backend/src/test/java/com/kaspi/backend/dao/UserDaoTest.java
@@ -0,0 +1,30 @@
+package com.kaspi.backend.dao;
+
+import com.kaspi.backend.domain.Age;
+import com.kaspi.backend.domain.Gender;
+import com.kaspi.backend.domain.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class UserDaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    @DisplayName("유저 insert,select query 테스트")
+    public void testInsertUser() {
+        //given
+        User user = new User.builder().userNo(1L).age(Age.TWENTY).gender(Gender.MALE).id("test").password("testpwd").build();
+        //when
+        userDao.insertUser(user.getId(), user.getPassword(), user.getGender().name(), user.getAge().name());
+        //then
+        User findUser = userDao.findByUserNo(user.getUserNo()).get();
+        assertThat(findUser).usingRecursiveComparison().isEqualTo(user);
+    }
+}
