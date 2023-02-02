0dd31761c002ed6db5fdc463f147019b4c31c58a
ajongs <wonjong1030@naver.com>
2023-02-02 16:52:19 +0900

    #4 feat(GasStationDataDao): GasStation 객체를 디비에 삽입하는 기능 구현

--- backend/src/main/java/com/kaspi/backend/dao/GasStationDataDao.java
+++ backend/src/main/java/com/kaspi/backend/dao/GasStationDataDao.java
@@ -1,2 +1,25 @@
-package com.kaspi.backend.dao;public class GasStationDataDao {
+package com.kaspi.backend.dao;
+
+import com.kaspi.backend.domain.GasDetail;
+import com.kaspi.backend.domain.GasStation;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class GasStationDataDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public GasStationDataDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void insertGasStation(GasStation gasStation) {
+        String query = "INSERT INTO gas_station(station_no, area, name, address, brand, is_self) " +
+                "values (?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(query, gasStation.getGasStationNo(), gasStation.getArea(), gasStation.getName(),
+                gasStation.getAddress(), gasStation.getBrand(), gasStation.getIsSelf());
+    }
 }
