7acad863b77fc3907f0c1cdfb7ee4db00cb8366c
rentalhub-mac78 <kksy2423@naver.com>
2023-02-03 18:21:57 +0900

    #17 feat(controller): 회원가입시 세션 redis에 저장 및 쿠키 관리 초기 세팅

--- backend/src/main/java/com/kaspi/backend/controller/UserController.java
+++ backend/src/main/java/com/kaspi/backend/controller/UserController.java
@@ -4,18 +4,29 @@ import com.kaspi.backend.dao.UserDao;
 import com.kaspi.backend.dto.SignUpRequestDto;
 import com.kaspi.backend.util.response.CommonResponseDto;
 import com.kaspi.backend.util.response.code.DefaultCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
 
     private final UserDao userDao;
+
+    @Autowired
+    private  RedisTemplate redisTemplate;
+    @Autowired
+    private SessionRepository sessionRepository;
 
     public UserController(UserDao userDao) {
         this.userDao = userDao;
@@ -24,12 +35,27 @@ public class UserController {
 
     @PostMapping("")
     public ResponseEntity<CommonResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
-        userDao.insertUser(signUpRequestDto.getId(),
+        Long userId = userDao.insertUser(signUpRequestDto.getId(),
                 signUpRequestDto.getPassword(),
                 signUpRequestDto.getGender(),
                 signUpRequestDto.getAge()
         );
 
+
+        Session session = sessionRepository.createSession();
+        session.setAttribute("userId",userId);
+        sessionRepository.save(session);
+        Session byId = sessionRepository.findById(session.getId());
+
+
+
+        ResponseCookie responseCookie = ResponseCookie.from("sid", session.getId())
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(60)
+                .domain("yourdomain.net")
+                .build();
         return new ResponseEntity(
                 CommonResponseDto.toResponse(DefaultCode.SUCCESS_SIGNUP),
                 HttpStatus.CREATED
