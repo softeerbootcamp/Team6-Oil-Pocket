86a7577328606ef43cf39026d759f3b8daaecb4c
gyuturn <rbtjrrbals@gmail.com>
2023-02-04 16:21:37 +0900

    #17 feat&test(enum): 요청 데이터에 대한 gender 리턴

--- backend/src/main/java/com/kaspi/backend/enums/Gender.java
+++ backend/src/main/java/com/kaspi/backend/enums/Gender.java
@@ -1,8 +1,17 @@
 package com.kaspi.backend.enums;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public enum Gender {
     MALE,
     FEMALE;
+
+    public static Optional<Gender> getGender(String requestGender) {
+        return Arrays.stream(values())
+                .filter(value -> value.name().equals(requestGender))
+                .findAny();
+    }
 
 
 }
---
+++ backend/src/test/java/com/kaspi/backend/enums/GenderTest.java
@@ -0,0 +1,23 @@
+package com.kaspi.backend.enums;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class GenderTest {
+
+    @Test
+    @DisplayName("요청데이터에 대한 gender객체 변환 테스트")
+    void getGender() {
+        //given
+        String requestGender = "MALE";
+        //when
+        Optional<Gender> gender = Gender.getGender(requestGender);
+        //then
+        assertThat(gender.isPresent()).isTrue();
+        assertThat(gender.get()).isEqualTo(Gender.MALE);
+    }
+}
