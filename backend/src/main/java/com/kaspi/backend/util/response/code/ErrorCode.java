5823bb71f6aba74b841d54a476e3d533cdcbcc42
gyuturn <rbtjrrbals@gmail.com>
2023-02-05 01:23:00 +0900

    #28 feat(code): 쿠키에 세션이 만료된경우나 없는경우를 위한 NotValidUserException 에러 생성

--- backend/src/main/java/com/kaspi/backend/util/response/code/ErrorCode.java
+++ backend/src/main/java/com/kaspi/backend/util/response/code/ErrorCode.java
@@ -2,7 +2,8 @@ package com.kaspi.backend.util.response.code;
 
 public enum ErrorCode implements Code {
     PARAMETER_ERROR("E001", "요청 데이터 형식이 올바르지 않습니다."),
-    AUTH_ERROR("E002", "세션이 만료되었습니다;");
+    AUTH_ERROR("E002", "세션이 만료되었습니다"),
+    NOT_VALID_USER("E003", "유저가 존재하지 않습니다.");
 
 
     private String code;
