37fc1d119a60df155ab7150124153c6ed8e893c9
gyuturn <rbtjrrbals@gmail.com>
2023-02-05 01:31:31 +0900

    #28 test(service): 세션으로부터 유저를 가지고 오늘 로직 테스트

--- backend/src/test/java/com/kaspi/backend/service/HttpSessionServiceTest.java
+++ backend/src/test/java/com/kaspi/backend/service/HttpSessionServiceTest.java
@@ -1,9 +1,16 @@
 package com.kaspi.backend.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.kaspi.backend.dao.UserDao;
+import com.kaspi.backend.domain.User;
+import java.util.Optional;
 import javax.servlet.http.HttpSession;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,11 +22,14 @@ public class HttpSessionServiceTest {
 
     @Mock
     private HttpSession httpSession;
-
+    @Mock
+    private UserDao userDao;
     @InjectMocks
     private HttpSessionService httpSessionService;
 
+
     @Test
+    @DisplayName("세션 생성 로직 테스트")
     public void testMakeHttpSession() {
         // Given
         Long userId = 1L;
@@ -27,5 +37,23 @@ public class HttpSessionServiceTest {
         httpSessionService.makeHttpSession(userId);
         // Then
         verify(httpSession).setAttribute(HttpSessionService.SESSION_KEY, userId);
+    }
+
+    @Test
+    @DisplayName("세션으로부터 유저를 가지고 오는 로직 테스트")
+    void getUserFromSession() {
+        // Given
+        Long userNo = 1L;
+        when(httpSession.getAttribute(HttpSessionService.SESSION_KEY)).thenReturn(userNo);
+        User user = User.builder().userNo(userNo).build();
+        when(userDao.findById(userNo)).thenReturn(Optional.of(user));
+
+        // When
+        User resultUser = httpSessionService.getUserFromSession();
+
+        // Then
+        verify(httpSession).getAttribute(HttpSessionService.SESSION_KEY);
+        verify(userDao).findById(userNo);
+        assertThat(user).isEqualTo(resultUser);
     }
 }
