581d42101c1cefb818e5666eb38db3fc9c7b9db9
gyuturn <rbtjrrbals@gmail.com>
2023-02-04 17:16:22 +0900

    #17 feat(controller): 회원가입 api

--- backend/src/main/java/com/kaspi/backend/controller/UserController.java
+++ backend/src/main/java/com/kaspi/backend/controller/UserController.java
@@ -1,16 +1,16 @@
 package com.kaspi.backend.controller;
 
-import com.kaspi.backend.dao.UserDao;
+import static com.google.common.net.HttpHeaders.SET_COOKIE;
+
 import com.kaspi.backend.dto.SignUpRequestDto;
+import com.kaspi.backend.service.UserService;
 import com.kaspi.backend.util.response.CommonResponseDto;
 import com.kaspi.backend.util.response.code.DefaultCode;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
+import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.session.Session;
-import org.springframework.session.SessionRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,47 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    private final UserDao userDao;
-
-    @Autowired
-    private  RedisTemplate redisTemplate;
-    @Autowired
-    private SessionRepository sessionRepository;
-
-    public UserController(UserDao userDao) {
-        this.userDao = userDao;
-    }
-
+    private final UserService userService;
 
     @PostMapping("")
-    public ResponseEntity<CommonResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
-        Long userId = userDao.insertUser(signUpRequestDto.getId(),
-                signUpRequestDto.getPassword(),
-                signUpRequestDto.getGender(),
-                signUpRequestDto.getAge()
-        );
-
-
-        Session session = sessionRepository.createSession();
-        session.setAttribute("userId",userId);
-        sessionRepository.save(session);
-        Session byId = sessionRepository.findById(session.getId());
-
-
-
-        ResponseCookie responseCookie = ResponseCookie.from("sid", session.getId())
-                .httpOnly(true)
-                .secure(true)
-                .path("/")
-                .maxAge(60)
-                .domain("yourdomain.net")
-                .build();
-        return new ResponseEntity(
-                CommonResponseDto.toResponse(DefaultCode.SUCCESS_SIGNUP),
-                HttpStatus.CREATED
-                );
+    public ResponseEntity<CommonResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto,
+                                                    HttpSession session) {
+        Long userNo = userService.makeUser(signUpRequestDto);
+        ResponseCookie responseSessionCookie = userService.makeCookieFromSession(userNo, session);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header(SET_COOKIE, responseSessionCookie.toString())
+                .body(CommonResponseDto.toResponse(DefaultCode.SUCCESS_SIGNUP));
     }
+
 
 }
