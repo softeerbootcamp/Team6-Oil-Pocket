e6f0bf3796e2839f076f57784370156ac32220ab
ajongs <wonjong1030@naver.com>
2023-02-04 21:49:23 +0900

    #4 feat(GasDataService): 캐싱 기능 추가
    도로명 주소와 건물번호, 주유소 브랜드의 조합으로 key값을 만들고 주유소를 식별합니다.
    cacheMap에는 이 키값으로 db에 접근하지 않고 GasStationDto를 조회할 수 있습니다.
    오늘 하루에 대한 가격정보만을 취급합니다.

--- backend/src/main/java/com/kaspi/backend/domain/GasStationDto.java
+++ backend/src/main/java/com/kaspi/backend/domain/GasStationDto.java
@@ -1,2 +1,72 @@
-package com.kaspi.backend.domain;public class GasStationDTO {
+package com.kaspi.backend.domain;
+
+import java.util.List;
+
+public class GasStationDto {
+    private String area;
+    private String name;
+    private String address;
+    private String brand;
+    private boolean self;
+    private List<GasDetailDto> details;
+
+    public GasStationDto(String area, String name, String address, String brand, boolean self, List<GasDetailDto> details) {
+        this.area = area;
+        this.name = name;
+        this.address = address;
+        this.brand = brand;
+        this.self = self;
+        this.details = details;
+    }
+    public static GasStationDto newInstance(GasStation gasStation, List<GasDetailDto> list) {
+        return new GasStationDto(gasStation.getArea(), gasStation.getName(), gasStation.getAddress(), gasStation.getBrand(), gasStation.getIsSelf(), list);
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public boolean isSelf() {
+        return self;
+    }
+
+    public void setSelf(boolean self) {
+        this.self = self;
+    }
+
+    public List<GasDetailDto> getDetails() {
+        return details;
+    }
+
+    public void setDetails(List<GasDetailDto> details) {
+        this.details = details;
+    }
 }
--- backend/src/main/java/com/kaspi/backend/service/GasDataService.java
+++ backend/src/main/java/com/kaspi/backend/service/GasDataService.java
@@ -1,18 +1,27 @@
 package com.kaspi.backend.service;
 
 import com.kaspi.backend.dao.GasStationDataDao;
+import com.kaspi.backend.domain.GasDetail;
+import com.kaspi.backend.domain.GasDetailDto;
 import com.kaspi.backend.domain.GasStation;
+import com.kaspi.backend.domain.GasStationDto;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.kaspi.backend.util.DataDownloadScheduler.GAS_STATION;
 
 @Service
 public class GasDataService {
     private final GasStationDataDao gasStationDataDao;
-    private final List<String> cache = new ArrayList<>();
+    private final Map<String, GasStation> gasStationInfos = new HashMap<>();
+    private final Map<String, GasStationDto> cacheMap = new ConcurrentHashMap<>();
 
     public GasDataService(GasStationDataDao gasStationDataDao) {
         this.gasStationDataDao = gasStationDataDao;
@@ -27,14 +36,38 @@ public class GasDataService {
         while ((line = br.readLine()) != null) {
             String[] attribute = line.split(",");
             if (attribute.length <= 1) break;
-            GasStation gasStation = GasStation.parseGasStation(attribute);
-            if (!cache.contains(gasStation.getGasStationNo())) {
-                System.out.println(line);
-                cache.add(gasStation.getGasStationNo());
+
+            String key = makeCacheKey(attribute);
+            System.out.println(key);
+
+            //가스 도로명+브랜드 겹치는거 있는지 체크
+            if (!gasStationInfos.containsKey(key)) {
+                GasStation gasStation = GasStation.parseGasStation(attribute);
+                gasStationInfos.put(key, gasStation);
                 gasStationDataDao.insertGasStation(gasStation);
             }
-            callback.makeGasDetailAndSaveToDB(gasStationDataDao, attribute, date);
+            attribute[0] = gasStationInfos.get(key).getGasStationNo();
+            List<GasDetail> gasDetails = callback.makeGasDetailAndSaveToDB(gasStationDataDao, attribute, date);
+            cacheMap.put(key, GasStationDto.newInstance(gasStationInfos.get(key), GasDetailDto.newDtoList(gasDetails)));
         }
         file.delete();
+    }
+    private String makeCacheKey(String[] attribute) {
+        return makeEndAddress(attribute[3]) + ":" + attribute[4];
+    }
+    private String makeEndAddress(String address) {
+        String[] temp = address.split(" ");
+        int idx = 0;
+        for (int i = 0; i < temp.length; i++) {
+            if (temp[i].contains("로") || temp[i].contains("길")) {
+                idx = i;
+                break;
+            }
+        }
+        String result = "";
+        for (int i = idx; i < temp.length; i++) {
+            result += temp[i] + " ";
+        }
+        return result;
     }
 }
