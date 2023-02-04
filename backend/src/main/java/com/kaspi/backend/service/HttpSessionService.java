86522aad570f985d629995d111e5b787b2114a0e
gyuturn <rbtjrrbals@gmail.com>
2023-02-05 01:31:43 +0900

    #28 feat(service): 세션으로부터 유저를 가지고 오늘 로직 추가

--- backend/src/main/java/com/kaspi/backend/service/HttpSessionService.java
+++ backend/src/main/java/com/kaspi/backend/service/HttpSessionService.java
@@ -1,6 +1,10 @@
 package com.kaspi.backend.service;
 
+import com.kaspi.backend.dao.UserDao;
 import com.kaspi.backend.domain.User;
+import com.kaspi.backend.util.exception.NotValidUserException;
+import com.kaspi.backend.util.response.code.ErrorCode;
+import java.util.Optional;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,13 +12,23 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class HttpSessionService {
-    public static final String SESSION_KEY = "userId";
+    public static final String SESSION_KEY = "userNo";
 
     private final HttpSession httpSession;
+    private final UserDao userDao;
 
 
     public void makeHttpSession(Long userId) {
         httpSession.setAttribute(SESSION_KEY, userId);
+    }
+
+    public User getUserFromSession() {
+        Long userNo = (Long) httpSession.getAttribute(SESSION_KEY);
+        Optional<User> findUser = userDao.findById(userNo);
+        if (findUser.isEmpty()) {
+            throw new NotValidUserException(ErrorCode.NOT_VALID_USER);
+        }
+        return findUser.get();
     }
 
 
