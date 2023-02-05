66e66958cb3150d864674ba49439ca46c7ab2dbe
gyuturn <rbtjrrbals@gmail.com>
2023-02-05 23:25:17 +0900

    #28 feat(*): 로그인 API

--- backend/src/main/java/com/kaspi/backend/controller/AuthController.java
+++ backend/src/main/java/com/kaspi/backend/controller/AuthController.java
@@ -1,14 +1,22 @@
 package com.kaspi.backend.controller;
 
 
+import com.kaspi.backend.dao.UserDao;
+import com.kaspi.backend.domain.User;
+import com.kaspi.backend.dto.SignInRequestDto;
 import com.kaspi.backend.service.AuthService;
+import com.kaspi.backend.service.HttpSessionService;
+import com.kaspi.backend.util.exception.AuthorizationException;
 import com.kaspi.backend.util.response.CommonResponseDto;
 import com.kaspi.backend.util.response.code.DefaultCode;
 import com.kaspi.backend.util.response.code.ErrorCode;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final HttpSessionService httpSessionService;
 
     @GetMapping("/v1/auth")
     public ResponseEntity<CommonResponseDto> checkDuplicateId(@RequestParam("id") String id) {
@@ -28,5 +37,15 @@ public class AuthController {
         }
         return ResponseEntity.status(HttpStatus.OK)
                 .body(CommonResponseDto.toResponse(DefaultCode.SUCCESS_VALID_ID));
+    }
+
+    @PostMapping("/v1/auth")
+    public ResponseEntity<CommonResponseDto> signIn(@RequestBody SignInRequestDto signInRequestDto) {
+
+        User authenticatedUser = authService.signIn(signInRequestDto);
+        httpSessionService.makeHttpSession(authenticatedUser.getUserNo());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(CommonResponseDto.toResponse(DefaultCode.SUCCESS_SIGN_IN));
     }
 }
