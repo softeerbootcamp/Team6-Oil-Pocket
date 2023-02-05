b093691eccddc9fd9a4e68271ab1e0d69c2d9f12
moon-GD <king4mun@gmail.com>
2023-02-05 11:39:30 +0900

    add#6feat(navbar):navbar css cursor pointer 변경

--- frontEnd/public/views/navBarView.js
+++ frontEnd/public/views/navBarView.js
@@ -3,7 +3,7 @@ const navBarView = `
         <div class="navbar__logo"><a href="/">Oil Pocket<a/></div>
         <div class="navbar__btns">
             <span><a href="/userDetail">jaewon님 안녕하세요</a></span>
-            <span> / </span>
+            <span> | </span>
             <span class="navbar__myPageBtn"><a href="/login" data-link>마이 페이지</a></span>
             <span> | </span>
             <span>한국</span>
