f64c77a7bb3f4491640fad09de0c3a68787f4bff
ajongs <wonjong1030@naver.com>
2023-02-05 19:35:10 +0900

    #4 feat(GasDetailCallback): 오일(휘발유,경유) 종류의 csv파일을 파싱하여 db에 삽입하는 기능 추가

--- util/src/main/java/org/example/service/NomalGasDetailCallback.java
+++ util/src/main/java/org/example/service/NomalGasDetailCallback.java
@@ -1,15 +1,14 @@
-package com.kaspi.backend.service;
+package org.example.service;
 
-import com.kaspi.backend.dao.GasStationDataDao;
-import com.kaspi.backend.domain.GasDetail;
+import org.example.dao.GasStationDataDao;
+import org.example.domain.GasDetail;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public class NomalGasDetailCallback implements GasDetailCallback{
     @Override
-    public List<GasDetail> makeGasDetailAndSaveToDB(GasStationDataDao gasStationDataDao, String[] attribute, LocalDate date) {
-        List<GasDetail> gasDetailList = GasDetail.parseListGasDetail(attribute, date);
+    public List<GasDetail> makeGasDetailAndSaveToDB(GasStationDataDao gasStationDataDao, String[] attribute) {
+        List<GasDetail> gasDetailList = GasDetail.parseListGasDetail(attribute);
         gasStationDataDao.insertGasDetails(gasDetailList);
         return gasDetailList;
     }
