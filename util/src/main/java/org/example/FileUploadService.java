4d890046a6af754ba45b6c173a52839ff499c87c
ajongs <wonjong1030@naver.com>
2023-02-05 23:56:57 +0900

    #4 feat(FileUploadService): csv 파일을 한줋씩 읽고 db에 삽입하는 기능 추가
    먼저 주유소 레코드가 중복된 것이 있는지 확인하고 없다면 삽입합니다
    다시 db까지 가지 않도록 레코드의 유무를 gasStationInfos Map에 저장해놓고 캐시를 합니다
    GasDetail은 항상 삽입합니다

--- util/src/main/java/org/example/FileUploadService.java
+++ util/src/main/java/org/example/FileUploadService.java
@@ -19,11 +19,49 @@ public class FileUploadService {
     private static final String OIL_FILE = "과거_판매가격(주유소)";
     private static final String LPG_FILE = "과거_판매가격(충전소)";
     private final Map<String, GasStation> gasStationInfos = new HashMap<>();
-    public FileUploadService() {}
+    private GasStationDataDao gasStationDataDao = new GasStationDataDao();
+
+    public FileUploadService() {
+    }
+
+    public void fileRead(File file, GasDetailCallback callback) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "euc-kr"));
+            br.readLine();
+            br.readLine(); //두줄은 데이터 제목들이어서 건너뜁니다.
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                line = removeDoubleQuotationMarks(line);
+                String[] attribute = line.split(",");
+                if (attribute.length <= 1) break;
+                attribute[3] = extractRoadNameAndBuildingNum(attribute[3]);
+                String key = makeCacheKey(attribute);
+
+                //키 값이 겹치는 지 확인
+                if (!gasStationInfos.containsKey(key)) {
+                    GasStation gasStation = GasStation.parseGasStation(attribute);
+                    gasStationInfos.put(key, gasStation);
+                    if (gasStationDataDao.selectGasStation(gasStation) == null) {
+                        gasStationDataDao.insertGasStation(gasStation);
+                    }
+                }
+                GasStation gasStation = gasStationInfos.get(key);
+                attribute[0] = gasStation.getGasStationNo();
+                callback.makeGasDetailAndSaveToDB(gasStationDataDao, attribute);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("파일을 찾을 수 없습니다.");
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public String removeDoubleQuotationMarks(String line) {
         return line.replaceAll("\"", "");
     }
+
     private String extractRoadNameAndBuildingNum(String address) {
         String[] temp = address.split(" |\\(");
         int idx = 0;
@@ -42,6 +80,7 @@ public class FileUploadService {
         }
         return result.trim();
     }
+
     public String makeCacheKey(String[] attribute) {
         return attribute[3] + ":" + attribute[5];
     }
