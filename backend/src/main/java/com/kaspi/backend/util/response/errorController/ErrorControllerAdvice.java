2c4cdaa371ac26bece0220783899ab69b0dc3b65
gyuturn <rbtjrrbals@gmail.com>
2023-02-06 00:03:30 +0900

    #28 feat(response): IllegalArgumentException 컨트롤러 예외 처리

--- backend/src/main/java/com/kaspi/backend/util/response/errorController/ErrorControllerAdvice.java
+++ backend/src/main/java/com/kaspi/backend/util/response/errorController/ErrorControllerAdvice.java
@@ -3,6 +3,7 @@ package com.kaspi.backend.util.response.errorController;
 import com.kaspi.backend.util.exception.AuthenticationException;
 import com.kaspi.backend.util.exception.AuthorizationException;
 import com.kaspi.backend.util.response.CommonResponseDto;
+import com.kaspi.backend.util.response.code.ErrorCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,13 @@ public class ErrorControllerAdvice {
         log.error(e.getCode().getMessage());
         e.printStackTrace();
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(CommonResponseDto.toResponse(e.getCode()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<CommonResponseDto> IllegalArgumentExceptionHandler(IllegalArgumentException e) {
+        log.error(e.getMessage());
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(CommonResponseDto.toResponse(ErrorCode.PARAMETER_ERROR));
     }
 
 //    @ExceptionHandler(AuthorizationException.class)
