825c12e1859137bb733afff03d959344ce849a13
gyuturn <rbtjrrbals@gmail.com>
2023-02-05 18:40:53 +0900

    #28 test(dao): 사용자 아이디 중복확인을 위한 쿼리 테스트

---
+++ backend/src/test/java/com/kaspi/backend/dao/UserDaoTest.java
@@ -0,0 +1,40 @@
+package com.kaspi.backend.dao;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.kaspi.backend.domain.User;
+import com.kaspi.backend.enums.Age;
+import com.kaspi.backend.enums.Gender;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class UserDaoTest {
+
+    @Autowired
+    private UserDao userDao;
+
+
+    @Test
+    @DisplayName("사용자 아이디를 조건으로 user존재하는지 쿼리 테스트")
+    void findByUserId() {
+        //given
+        String userId = "test";
+        User existUser = User.builder()
+                .id(userId)
+                .password("password")
+                .gender(Gender.MALE)
+                .age(Age.TWENTY)
+                .build();
+        userDao.save(existUser);
+        //when
+        Optional<User> findUser = userDao.findByUserId(userId);
+        //then
+        assertThat(findUser.isPresent()).isTrue();
+        assertThat(findUser.get()).usingRecursiveComparison().isEqualTo(existUser);
+    }
+}
