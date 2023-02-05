6ec1662e247b97c96a7f38eae0b2c4c834b34f17
ajongs <wonjong1030@naver.com>
2023-02-05 17:05:18 +0900

    #4 feat(GasStationController): mock api 추가
    임시로 만든 mock api입니다 아직 사용하지 말아주세요

--- backend/src/main/java/com/kaspi/backend/controller/GasStationController.java
+++ backend/src/main/java/com/kaspi/backend/controller/GasStationController.java
@@ -1,2 +1,28 @@
-package com.kaspi.backend.controller;public class GasStationController {
+package com.kaspi.backend.controller;
+
+import com.kaspi.backend.domain.GasDetailDto;
+import com.kaspi.backend.domain.GasStationDto;
+import com.kaspi.backend.enums.GasType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/gasStation")
+public class GasStationController {
+    @GetMapping
+    public ResponseEntity<GasStationDto> getGasInfos() {
+        GasDetailDto gasDetailDto = new GasDetailDto(GasType.GASOLINE, 2000, LocalDate.now());
+        List<GasDetailDto> list = new ArrayList<>();
+        list.add(gasDetailDto);
+        GasStationDto gasStationDto = new GasStationDto( "서울 종로구", "㈜지에스이앤알 평창주유소",
+                "서울 종로구 평창문화로 135 (평창동)", "현대오일뱅크", true, list);
+        return new ResponseEntity<>(gasStationDto, HttpStatus.OK);
+    }
 }
