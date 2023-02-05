b59e4265f6ca8a9547062299e67a0ed3bac28291
ajongs <wonjong1030@naver.com>
2023-02-05 19:36:28 +0900

    #4 fix(GasDetail): 데이터의 속성 인덱스를 수정
    인덱스가 맞지 않아서 기대했던 대로 db에 삽입되지 않는 버그 수정

--- util/src/main/java/org/example/domain/GasDetail.java
+++ util/src/main/java/org/example/domain/GasDetail.java
@@ -3,16 +3,18 @@ package org.example.domain;
 import org.example.enums.GasType;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GasDetail {
     private static final int ADDRESS = 3;
-    private static final int BRAND = 4;
-    private static final int PREMIUM_GASOLINE = 6;
-    private static final int GASOLINE = 7;
-    private static final int DIESEL = 8;
-    private static final int LPG = 6;
+    private static final int DATE = 4;
+    private static final int BRAND = 5;
+    private static final int PREMIUM_GASOLINE = 7;
+    private static final int GASOLINE = 8;
+    private static final int DIESEL = 9;
+    private static final int LPG = 7;
     private String address;
     private String brand;
     private int price;
@@ -67,15 +69,15 @@ public class GasDetail {
         this.date = date;
     }
 
-    public static List<GasDetail> parseListGasDetail(String[] attribute, LocalDate date) {
+    public static List<GasDetail> parseListGasDetail(String[] attribute) {
         List<GasDetail> list = new ArrayList<>();
-        list.add(new GasDetail(attribute[ADDRESS], attribute[BRAND], Integer.valueOf(attribute[PREMIUM_GASOLINE]), GasType.PREMIUM_GASOLINE, date));
-        list.add(new GasDetail(attribute[ADDRESS], attribute[BRAND], Integer.valueOf(attribute[GASOLINE]), GasType.GASOLINE, date));
-        list.add(new GasDetail(attribute[ADDRESS], attribute[BRAND], Integer.valueOf(attribute[DIESEL]), GasType.DIESEL, date));
+        list.add(new GasDetail(attribute[ADDRESS], attribute[BRAND], Integer.valueOf(attribute[PREMIUM_GASOLINE]), GasType.PREMIUM_GASOLINE, LocalDate.parse(attribute[DATE], DateTimeFormatter.ofPattern("yyyyMMdd"))));
+        list.add(new GasDetail(attribute[ADDRESS], attribute[BRAND], Integer.valueOf(attribute[GASOLINE]), GasType.GASOLINE, LocalDate.parse(attribute[DATE], DateTimeFormatter.ofPattern("yyyyMMdd"))));
+        list.add(new GasDetail(attribute[ADDRESS], attribute[BRAND], Integer.valueOf(attribute[DIESEL]), GasType.DIESEL, LocalDate.parse(attribute[DATE], DateTimeFormatter.ofPattern("yyyyMMdd"))));
         return list;
     }
 
-    public static GasDetail parseLpgGasDetail(String[] attribute, LocalDate date) {
-        return new GasDetail(attribute[ADDRESS], attribute[BRAND], Integer.valueOf(attribute[LPG]), GasType.LPG, date);
+    public static GasDetail parseLpgGasDetail(String[] attribute) {
+        return new GasDetail(attribute[ADDRESS], attribute[BRAND], Integer.valueOf(attribute[LPG]), GasType.LPG, LocalDate.parse(attribute[DATE], DateTimeFormatter.ofPattern("yyyyMMdd")));
     }
 }
