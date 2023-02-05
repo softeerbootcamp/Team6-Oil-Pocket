d1bdd3984fc366685fb546b66505f41d12b86342
gyuturn <rbtjrrbals@gmail.com>
2023-02-05 23:41:36 +0900

    #28 feat(response): 전역 에러 컨트롤러 생성

---
+++ backend/src/main/java/com/kaspi/backend/util/response/errorController/ErrorControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.kaspi.backend.util.response.errorController;
+
+import com.kaspi.backend.util.exception.AuthenticationException;
+import com.kaspi.backend.util.exception.AuthorizationException;
+import com.kaspi.backend.util.response.CommonResponseDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorControllerAdvice {
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<CommonResponseDto> AuthenticationHandler(AuthenticationException e) {
+        log.error(e.getCode().getMessage());
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(CommonResponseDto.toResponse(e.getCode()));
+    }
+
+//    @ExceptionHandler(AuthorizationException.class)
+//    public ResponseEntity<CommonResponseDto> AuthorizationHandler(AuthorizationException e) {
+//        log.error(e.getCode().getMessage());
+//        e.printStackTrace();
+//        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(CommonResponseDto.toResponse(e.getCode()));
+//    }
+}
