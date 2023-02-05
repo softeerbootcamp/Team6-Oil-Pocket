6718551ec69d61cb97b7720f06e1ffb0f2f9a8d4
gyuturn <rbtjrrbals@gmail.com>
2023-02-05 22:48:06 +0900

    #28 rename(filter): 인가의 의미를 갖도록 명명

--- backend/src/main/java/com/kaspi/backend/util/filter/AuthorizationFilter.java
+++ backend/src/main/java/com/kaspi/backend/util/filter/AuthorizationFilter.java
@@ -1,21 +1,15 @@
 package com.kaspi.backend.util.filter;
 
-import com.kaspi.backend.dao.UserDao;
-import com.kaspi.backend.domain.User;
-import com.kaspi.backend.service.UserService;
 import com.kaspi.backend.util.exception.NotAuthException;
 import com.kaspi.backend.util.response.code.ErrorCode;
 import javax.servlet.*;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.session.Session;
-import org.springframework.session.SessionRepository;
 
 @RequiredArgsConstructor
-public class AuthenticationFilter implements Filter {
+public class AuthorizationFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
--- backend/src/main/java/com/kaspi/backend/util/filter/FilterConfig.java
+++ backend/src/main/java/com/kaspi/backend/util/filter/FilterConfig.java
@@ -1,20 +1,16 @@
 package com.kaspi.backend.util.filter;
 
-import com.kaspi.backend.dao.UserDao;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.DispatcherType;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.session.SessionRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FilterConfig {
 
     @Bean
-    public FilterRegistrationBean<AuthenticationFilter> authenticationFilter() {
-        FilterRegistrationBean<AuthenticationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new AuthenticationFilter());
+    public FilterRegistrationBean<AuthorizationFilter> authenticationFilter() {
+        FilterRegistrationBean<AuthorizationFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new AuthorizationFilter());
         registrationBean.addUrlPatterns("/api/v2/**");
         return registrationBean;
     }
