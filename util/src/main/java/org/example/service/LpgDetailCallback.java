6a6782bc90cfdaf6896e3f71c0c2b7d411541a4b
ajongs <wonjong1030@naver.com>
2023-02-06 23:15:34 +0900

    #4 feat(LpgDetailCallback): 상속받고 있는 인터페이스의 변경으로 인한 수정

--- util/src/main/java/org/example/service/LpgDetailCallback.java
+++ util/src/main/java/org/example/service/LpgDetailCallback.java
@@ -1,17 +1,18 @@
 package org.example.service;
 
-import org.example.dao.GasStationDataDao;
 import org.example.domain.GasDetail;
+import org.example.domain.GasStation;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class LpgDetailCallback implements GasDetailCallback {
     @Override
-    public List<GasDetail> makeGasDetailAndSaveToDB(GasStationDataDao gasStationDataDao, String[] attribute) {
+    public List<GasDetail> makeGasDetailAndSaveToDB(GasStation gasStation, CrudRepository crudRepository, String[] attribute) {
         List<GasDetail> list = new ArrayList<>();
-        GasDetail gasDetail = GasDetail.parseLpgGasDetail(attribute);
-        gasStationDataDao.insertGasDetail(gasDetail);
+        GasDetail gasDetail = GasDetail.parseLpgGasDetail(gasStation, attribute);
+        crudRepository.save(gasDetail);
         list.add(gasDetail);
         return list;
     }
