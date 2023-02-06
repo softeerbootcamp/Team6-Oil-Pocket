d3faa0cff1847544fde592f33c6d795b6f6148aa
ajongs <wonjong1030@naver.com>
2023-02-06 23:16:04 +0900

    #4 feat(NomalGasDetailCallback): 상속받고 있는 인터페이스의 수정으로 인한 변경

--- util/src/main/java/org/example/service/NomalGasDetailCallback.java
+++ util/src/main/java/org/example/service/NomalGasDetailCallback.java
@@ -1,15 +1,18 @@
 package org.example.service;
 
-import org.example.dao.GasStationDataDao;
 import org.example.domain.GasDetail;
+import org.example.domain.GasStation;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public class NomalGasDetailCallback implements GasDetailCallback{
     @Override
-    public List<GasDetail> makeGasDetailAndSaveToDB(GasStationDataDao gasStationDataDao, String[] attribute) {
-        List<GasDetail> gasDetailList = GasDetail.parseListGasDetail(attribute);
-        gasStationDataDao.insertGasDetails(gasDetailList);
+    public List<GasDetail> makeGasDetailAndSaveToDB(GasStation gasStation, CrudRepository crudRepository, String[] attribute) {
+        List<GasDetail> gasDetailList = GasDetail.parseListGasDetail(gasStation, attribute);
+        for (GasDetail gasDetail : gasDetailList) {
+            crudRepository.save(gasDetail);
+        }
         return gasDetailList;
     }
 }
