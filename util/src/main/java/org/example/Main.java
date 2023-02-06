b1a611455f6b0ef8a70e9981c47fe60bc5e588d2
ajongs <wonjong1030@naver.com>
2023-02-06 23:08:28 +0900

    #4 feat(util): gradle 프로젝트를 스프링부트 프로젝트로 변경(디펜던시 추가)
    spring data jdbc를 사용하기 위해 스프링부트 프로젝트로 변경했습니다

--- util/src/main/java/org/example/Main.java
+++ util/src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        FileUploadService fileUploadService = new FileUploadService();
-        fileUploadService.fileOpen();
+        SpringApplication.run(Main.class, args);
     }
 }
