a6bdfa2118a5203250370a124edabeef9f9bf1a6
ajongs <wonjong1030@naver.com>
2023-02-06 23:12:35 +0900

    #4 feat(AttributeIndex): csv 파일에서 각 속성들의 index값을 enum으로 관리

--- util/src/main/java/org/example/enums/AttributeIndex.java
+++ util/src/main/java/org/example/enums/AttributeIndex.java
@@ -1,2 +1,23 @@
-package org.example.enums;public enum AttributeIndex {
+package org.example.enums;
+
+public enum AttributeIndex {
+    AREA(1),
+    NAME(2),
+    ADDRESS(3),
+    DATE (4),
+    BRAND(5),
+    SELF(6),
+    PREMIUM_GASOLINE(7),
+    LPG(7),
+    GASOLINE(8),
+    DIESEL(9);
+    private int index;
+
+    AttributeIndex(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
 }
