60eea7617686efe51aae4d0899e2b5ab814eb090
ajongs <wonjong1030@naver.com>
2023-02-06 23:05:51 +0900

    #4 test(GasDetailTest): GasDetail.parseListGasDetail 파라미터 변경으로 인한 테스트 코드 수정

--- backend/src/test/java/com/kaspi/backend/domain/GasDetailTest.java
+++ backend/src/test/java/com/kaspi/backend/domain/GasDetailTest.java
@@ -15,16 +15,18 @@ class GasDetailTest {
     @Test
     @DisplayName("GasDetail 정적 팩터리 메소드 성공 테스트 (주유소)")
     void parseGasDetail() {
+        //given
         String[] input = new String[]{"A0000035", "서울 종로구", "㈜지에스이앤알 평창주유소", "서울 종로구 평창문화로 135 (평창동)"
                 , "현대오일뱅크", "셀프", "1899", "1659", "1759", "0"};
         System.out.println(input.length);
         LocalDate date = LocalDate.now();
-        List<GasDetail> gasDetail = GasDetail.parseListGasDetail(input, date);
+        GasStation gasStation = GasStation.parseGasStation(input);
+        List<GasDetail> gasDetail = GasDetail.parseListGasDetail(gasStation, input, date);
 
         List<GasDetail> list = new ArrayList<>();
-        list.add(new GasDetail("서울 종로구 평창문화로 135 (평창동)", "현대오일뱅크", 1899, GasType.PREMIUM_GASOLINE, date));
-        list.add(new GasDetail("서울 종로구 평창문화로 135 (평창동)", "현대오일뱅크", 1659, GasType.GASOLINE, date));
-        list.add(new GasDetail("서울 종로구 평창문화로 135 (평창동)", "현대오일뱅크", 1759, GasType.DIESEL, date));
+        list.add(new GasDetail(gasStation, 1899, GasType.PREMIUM_GASOLINE, date));
+        list.add(new GasDetail(gasStation, 1659, GasType.GASOLINE, date));
+        list.add(new GasDetail(gasStation, 1759, GasType.DIESEL, date));
         Assertions.assertThat(gasDetail).usingRecursiveComparison().isEqualTo(list);
     }
 }
