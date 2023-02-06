304f7b5b2be31f7e49c8aa829abd8007610a0486
ajongs <wonjong1030@naver.com>
2023-02-06 23:06:34 +0900

    #4 test(GasStationTest): GasStation.parseGasStation 메소드 파라미터 수정으로 인한 테스트 코드 수정

--- backend/src/test/java/com/kaspi/backend/domain/GasStationTest.java
+++ backend/src/test/java/com/kaspi/backend/domain/GasStationTest.java
@@ -15,10 +15,10 @@ class GasStationTest {
 
         String[] attribute = str1.split(",");
         GasStation testGasStation1 = GasStation.parseGasStation(attribute);
-        GasStation gasStation1 = new GasStation("A0000035", "서울 종로구", "㈜지에스이앤알 평창주유소", "서울 종로구 평창문화로 135 (평창동)","현대오일뱅크",true);
+        GasStation gasStation1 = new GasStation("서울 종로구", "㈜지에스이앤알 평창주유소", "서울 종로구 평창문화로 135 (평창동)","현대오일뱅크",true);
         attribute = str2.split(",");
         GasStation testGasStation2 = GasStation.parseGasStation(attribute);
-        GasStation gasStation2 = new GasStation("A0000045", "서울 종로구", "SK북악주유소", "서울 종로구 평창문화로 137 (평창동)","SK에너지",false);
+        GasStation gasStation2 = new GasStation ("서울 종로구", "SK북악주유소", "서울 종로구 평창문화로 137 (평창동)","SK에너지",false);
         SoftAssertions softAssertions = new SoftAssertions();
         softAssertions.assertThat(testGasStation1).usingRecursiveComparison().isEqualTo(gasStation1);
         softAssertions.assertThat(testGasStation2).usingRecursiveComparison().isEqualTo(gasStation2);
