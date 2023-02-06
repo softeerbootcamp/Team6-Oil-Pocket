3eb6446489d2c245679b4433ac5c1688eb0678f0
ajongs <wonjong1030@naver.com>
2023-02-06 22:55:37 +0900

    #4 feat(GasDetailDao): 레파지토리 추가

--- backend/src/main/java/com/kaspi/backend/dao/GasDetailDao.java
+++ backend/src/main/java/com/kaspi/backend/dao/GasDetailDao.java
@@ -1,2 +1,10 @@
-package com.kaspi.backend.dao;public interface GasDetailDao {
+package com.kaspi.backend.dao;
+
+import com.kaspi.backend.domain.GasDetail;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GasDetailDao extends CrudRepository<GasDetail, Long> {
+
 }
