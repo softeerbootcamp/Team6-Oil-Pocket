a8a71fc9d0ffe216a3cef5d3a7673891f3b570c7
rentalhub-mac78 <kksy2423@naver.com>
2023-02-07 10:23:25 +0900

    #40 feat(config): cors설정 헤더 전체 수용

--- backend/src/main/java/com/kaspi/backend/util/config/WebConfig.java
+++ backend/src/main/java/com/kaspi/backend/util/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173/")
-                .allowedMethods("*")
                 .allowedHeaders("*")
+                .allowedMethods("*")
                 .allowCredentials(true);
     }
 }
