e182ff903632544c5dd979d662bfc2964ebc33d9
rentalhub-mac78 <kksy2423@naver.com>
2023-02-07 15:35:55 +0900

    #40 feat(cookie): origin 프론트 포트삽입

--- backend/src/main/java/com/kaspi/backend/util/config/WebConfig.java
+++ backend/src/main/java/com/kaspi/backend/util/config/WebConfig.java
@@ -9,8 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") // 허용할 출처
-                .allowedOrigins("http://localhost:5173") // 허용할 출처
+                .allowedOrigins("http://localhost:8080","http://localhost:5173") // 허용할 출처
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
                 .allowCredentials(true); // 쿠키 인증 요청 허용
     }
