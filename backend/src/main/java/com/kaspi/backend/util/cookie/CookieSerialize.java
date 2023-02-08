2c4b1645540bf0483b42e7b26821ca147a57b9a8
rentalhub-mac78 <kksy2423@naver.com>
2023-02-08 18:36:12 +0900

    #87 remove: CookieSerialize 삭제

--- backend/src/main/java/com/kaspi/backend/util/cookie/CookieSerialize.java
+++ backend/src/main/java/com/kaspi/backend/util/cookie/CookieSerialize.java
@@ -1,21 +1,21 @@
-package com.kaspi.backend.util.cookie;
-
-import java.net.MalformedURLException;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.web.http.CookieSerializer;
-import org.springframework.session.web.http.DefaultCookieSerializer;
-
-/**
- * 쿠키 samesite 설정 none
- * 추후 배포시 삭제 예정
- */
-@Configuration
-public class CookieSerialize {
-    @Bean
-    public CookieSerializer cookieSerializer() throws MalformedURLException {
-        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-        serializer.setSameSite("None");
-        return serializer;
-    }
-}
+//package com.kaspi.backend.util.cookie;
+//
+//import java.net.MalformedURLException;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.session.web.http.CookieSerializer;
+//import org.springframework.session.web.http.DefaultCookieSerializer;
+//
+///**
+// * 쿠키 samesite 설정 none
+// * 추후 배포시 삭제 예정
+// */
+//@Configuration
+//public class CookieSerialize {
+//    @Bean
+//    public CookieSerializer cookieSerializer() throws MalformedURLException {
+//        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+//        serializer.setSameSite("None");
+//        return serializer;
+//    }
+//}
