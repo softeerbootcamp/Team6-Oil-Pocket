efea16d497e64c9ee7cd92ff7253da31736be9c5
ajongs <wonjong1030@naver.com>
2023-02-08 17:48:18 +0900

    #100 chore(build.gradle): h2 인메모리 디비 디펜던시 주석 처리

---
+++ backend/src/test/java/com/kaspi/backend/util/config/RedisConfiguration.java
@@ -0,0 +1,22 @@
+package com.kaspi.backend.util.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@TestConfiguration
+public class RedisConfiguration {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
---
+++ backend/src/test/java/com/kaspi/backend/util/config/TestRedisConfiguration.java
@@ -0,0 +1,2 @@
+package com.kaspi.backend.util.config;public class TestRedisConfiguration {
+}
