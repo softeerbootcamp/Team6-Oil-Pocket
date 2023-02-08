bb7a0b01a882c791298080a49408b3f9464a13f7
ajongs <wonjong1030@naver.com>
2023-02-08 17:50:07 +0900

    #100 feat(GasStationController): 목 컨트롤러 생성

--- backend/src/main/java/com/kaspi/backend/controller/GasStationController.java
+++ backend/src/main/java/com/kaspi/backend/controller/GasStationController.java
@@ -6,6 +6,7 @@ import com.kaspi.backend.enums.GasType;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +15,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/gasStation")
+@RequestMapping("/api/v1/gas-station")
 public class GasStationController {
-    @GetMapping
-    public ResponseEntity<GasStationDto> getGasInfos() {
+    @GetMapping("/{name}/{roadName}/{buildNum}/{brand}")
+    public ResponseEntity<GasStationDto> getGasInfos(@PathVariable("name") String name,
+                                                     @PathVariable("roadName") String roadName,
+                                                     @PathVariable("buildNum") String buildNum,
+                                                     @PathVariable("brand") String brand) {
         GasDetailDto gasDetailDto = new GasDetailDto(GasType.GASOLINE, 2000, LocalDate.now());
         List<GasDetailDto> list = new ArrayList<>();
         list.add(gasDetailDto);
+        GasStationDto gasStationDto = new GasStationDto( "서울 종로구", name,
+                roadName, buildNum, true, list);
+        /*
         GasStationDto gasStationDto = new GasStationDto( "서울 종로구", "㈜지에스이앤알 평창주유소",
-                "서울 종로구 평창문화로 135 (평창동)", "현대오일뱅크", true, list);
+                "서울 종로구 평창문화로", "현대오일뱅크", true, list);*/
         return new ResponseEntity<>(gasStationDto, HttpStatus.OK);
     }
 }
