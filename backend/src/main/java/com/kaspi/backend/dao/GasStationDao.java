498ff5dc8b5e51b93f40d834992aea38cfcee8a1
ajongs <wonjong1030@naver.com>
2023-02-08 18:30:37 +0900

    #100 feat(GasStationDao): like를 사용해서 GasStation을 조회하는 쿼리를 분리하였습니다
    자연어 처리가 불가하니까 일단 먼저 같은 도로명과 건물번호가 일치하는게 있다면 찾아내고 없을때만 like 쿼리를 쓸 예정입니다.

--- backend/src/main/java/com/kaspi/backend/dao/GasStationDao.java
+++ backend/src/main/java/com/kaspi/backend/dao/GasStationDao.java
@@ -7,9 +7,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
 @Repository
 public interface GasStationDao extends CrudRepository<GasStation, Long> {
 
-    @Query("select * from gas_station where address like :address and brand = :brand")
-    public Optional<GasStation> findByAddressAndBrand(@Param("address") String address, @Param("brand") String brand);
+    @Query("select * from gas_station where address = :address and brand = :brand")
+    Optional<GasStation> findByAddressAndBrand(@Param("address") String address, @Param("brand") String brand);
+
+    @Query("select * from gas_station where address like :address and brand =:brand ")
+    Optional<GasStation> findLikeByAddressAndBrand(@Param("address") String address, @Param("brand") String brand);
 }
