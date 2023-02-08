d2abf936844cbc8461bf9e3b33dd00736114550e
ajongs <wonjong1030@naver.com>
2023-02-08 17:56:07 +0900

    #100 test(TestRedisConfiguration): 테스트만을 위한 레디스 설정 테스트 추가

--- backend/src/test/java/com/kaspi/backend/util/config/TestRedisConfiguration.java
+++ backend/src/test/java/com/kaspi/backend/util/config/TestRedisConfiguration.java
@@ -1,2 +1,27 @@
-package com.kaspi.backend.util.config;public class TestRedisConfiguration {
+package com.kaspi.backend.util.config;
+
+import redis.embedded.RedisServer;
+import org.springframework.boot.test.context.TestConfiguration;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@TestConfiguration
+public class TestRedisConfiguration {
+
+    private RedisServer redisServer;
+
+    public TestRedisConfiguration() {
+        this.redisServer = new RedisServer(6379);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
 }
