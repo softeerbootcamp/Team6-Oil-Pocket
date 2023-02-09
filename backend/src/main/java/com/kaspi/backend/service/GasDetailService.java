56dd0ff2dc8c17d8e54cefe743ca478b6bcfcd5d
ajongs <wonjong1030@naver.com>
2023-02-09 19:01:01 +0900

    #100 feat(GasDetailService): GasDetail 리스트를 DTO 형식에 맞게 GasDetailDto 리스트로 변환하는 기능 추가

--- backend/src/main/java/com/kaspi/backend/service/GasDetailService.java
+++ backend/src/main/java/com/kaspi/backend/service/GasDetailService.java
@@ -2,10 +2,15 @@ package com.kaspi.backend.service;
 
 import com.kaspi.backend.dao.GasDetailDao;
 import com.kaspi.backend.dao.GasStationDao;
+import com.kaspi.backend.domain.GasDetail;
+import com.kaspi.backend.domain.GasDetailDto;
 import com.kaspi.backend.domain.GasStation;
+import com.kaspi.backend.enums.GasType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,6 +19,21 @@ public class GasDetailService {
     private static final String SPACE = " ";
     private static final String WILDCARD = "%";
     private final GasStationDao gasStationDao;
+    private final GasDetailDao gasDetailDao;
+
+    private List<GasDetailDto> convertToGasDetailDtoList(List<GasDetail> gasDetailList) {
+        List<GasDetailDto> gasDetailDtoList;
+        // 오일이 리스트에 있는 경우, LPG empty 상세정보 추가
+        if (gasDetailList.size() > 1) {
+            gasDetailDtoList = GasDetailDto.newDtoList(gasDetailList);
+            gasDetailDtoList.add(GasDetailDto.makeEmptyDetailDto(GasType.LPG));
+            return gasDetailDtoList;
+        }
+        // LPG 만 리스트에 있는 경우, 오일 종류들의 empty 상세정보를 추가
+        gasDetailDtoList = GasDetailDto.makeEmptyOilDetailDtoList();
+        gasDetailDtoList.add(gasDetailDtoList.get(0));
+        return gasDetailDtoList;
+    }
 
     public GasStation findGasStation(String roadNum, String buildNum, String brand) {
         String address = roadNum + SPACE + buildNum;
@@ -26,6 +46,7 @@ public class GasDetailService {
         }
         return optionalGasStation.get();
     }
+
     private String getLikeAddress(String roadNum, String buildNum) {
         return WILDCARD + roadNum + WILDCARD + buildNum;
     }
