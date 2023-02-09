3f8f0cb13eb0eb0e5f982a3530700eba0ce0ca4e
rentalhub-mac78 <kksy2423@naver.com>
2023-02-09 18:46:11 +0900

    #110 test(service): gas station 네이밍 검색 로직

---
+++ backend/src/main/java/com/kaspi/backend/service/GasStationService.java
@@ -0,0 +1,40 @@
+package com.kaspi.backend.service;
+
+import com.kaspi.backend.dao.GasStationDao;
+import com.kaspi.backend.domain.GasStation;
+import com.kaspi.backend.dto.FindGasStationReqDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class GasStationService {
+
+    private final GasStationDao gasStationDao;
+
+    public List<FindGasStationReqDto> getGasStationByContainingName(String reqGasStationName) {
+        List<FindGasStationReqDto> matchingGasStations = new ArrayList<>();
+        Iterable<GasStation> gasStations = gasStationDao.findAll();
+        for (GasStation gasStation : gasStations) {
+            insertMatchingGasStation(reqGasStationName, gasStation, matchingGasStations);
+        }
+        return matchingGasStations;
+    }
+
+    private void insertMatchingGasStation(String reqGasStationName, GasStation gasStation, List<FindGasStationReqDto> matchingGasStations) {
+        if (gasStation.getName().contains(reqGasStationName)) {
+            matchingGasStations.add(FindGasStationReqDto.builder()
+                    .stationNo(gasStation.getStationNo())
+                    .brand(gasStation.getBrand())
+                    .name(gasStation.getName())
+                    .area(gasStation.getArea())
+                    .address(gasStation.getAddress())
+                    .build());
+        }
+    }
+}
