5e34aaa1c1c15e5495f730badfdfd6e7062779dc
ajongs <wonjong1030@naver.com>
2023-02-09 17:47:30 +0900

    #100 feat(GasDetailService): 도로명, 건물번호, 브랜드로 GasStation을 찾을 수 있는 서비스 구현

--- backend/src/main/java/com/kaspi/backend/service/GasDetailService.java
+++ backend/src/main/java/com/kaspi/backend/service/GasDetailService.java
@@ -1,14 +1,31 @@
 package com.kaspi.backend.service;
 
+import com.kaspi.backend.dao.GasDetailDao;
+import com.kaspi.backend.dao.GasStationDao;
+import com.kaspi.backend.domain.GasStation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class GasDetailService {
     private static final String SPACE = " ";
     private static final String WILDCARD = "%";
+    private final GasStationDao gasStationDao;
 
+    public GasStation findGasStation(String roadNum, String buildNum, String brand) {
+        String address = roadNum + SPACE + buildNum;
+        Optional<GasStation> optionalGasStation = gasStationDao.findByAddressAndBrand(address, brand);
+        if (optionalGasStation.isEmpty()) {
+            optionalGasStation = gasStationDao.findByLikeAddressAndBrand(getLikeAddress(roadNum, buildNum), brand);
+            if (optionalGasStation.isEmpty()) {
+                throw new RuntimeException("존재하지 않는 주유소 입니다."); //TODO Exception 생성작업 해야함
+            }
+        }
+        return optionalGasStation.get();
+    }
     private String getLikeAddress(String roadNum, String buildNum) {
         return WILDCARD + roadNum + WILDCARD + buildNum;
     }
