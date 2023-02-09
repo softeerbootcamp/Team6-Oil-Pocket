f3b1da9c2df51d09ea5b46a26d85566870af2018
rentalhub-mac78 <kksy2423@naver.com>
2023-02-09 11:28:34 +0900

    #87 test(gradle): local환경에서 테스트를 위한 redis config 생성

--- backend/src/test/java/com/kaspi/backend/util/config/TestRedisConfiguration.java
+++ backend/src/test/java/com/kaspi/backend/util/config/TestRedisConfiguration.java
@@ -1,28 +1,23 @@
 package com.kaspi.backend.util.config;
 
-import redis.embedded.RedisServer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 @TestConfiguration
 public class TestRedisConfiguration {
+    @Value("${spring.redis.host}")
+    private String host;
 
-    private RedisServer redisServer;
+    @Value("${spring.redis.port}")
+    private int port;
 
-    public TestRedisConfiguration() {
-        this.redisServer = new RedisServer(6379);
-    }
-
-    @PostConstruct
-    public void postConstruct() {
-        redisServer.start();
-    }
-
-    @PreDestroy
-    public void preDestroy() {
-        redisServer.stop();
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(configuration);
     }
 }
