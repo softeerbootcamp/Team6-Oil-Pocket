0b1a50521438ea02a5e060551f3f0993e902a271
ajongs <wonjong1030@naver.com>
2023-02-09 15:44:04 +0900

    #100 feat(GasDetailDao): 주어진 주유소와 날짜에 해당하는 GasDetail 리스트를 반환하도록 findByStationNoAndDate 메소드를 수정
    고급유, 휘발유, 경유, LPG등 해당하는 모든 주유소가 반환될 수 있도록 수정"

--- backend/src/main/java/com/kaspi/backend/dao/GasDetailDao.java
+++ backend/src/main/java/com/kaspi/backend/dao/GasDetailDao.java
@@ -1,16 +1,17 @@
 package com.kaspi.backend.dao;
 
 import com.kaspi.backend.domain.GasDetail;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface GasDetailDao extends CrudRepository<GasDetail, Long> {
-    @Value("SELECT * FROM gas_detail where station_no = :stationNo and created_date = :date")
-    Optional<GasDetail> findByStationNoAndDate(@Param("stationNo") Long stationNo, @Param("date") LocalDate date);
+    @Query("select * from gas_detail where station_no = :stationNo and created_date = :date")
+    Optional<List<GasDetail>> findByStationNoAndDate(@Param("stationNo") Long stationNo, @Param("date") LocalDate date);
 }
