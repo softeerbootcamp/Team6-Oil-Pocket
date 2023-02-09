26576c275c0610ac3c7cc359b2dc467b01690958
ajongs <wonjong1030@naver.com>
2023-02-09 14:26:13 +0900

    #100 feat(GasDetailDao): findByStationNoAndDate 메소드 추가
    station넘버와 date를 기준으로 detail 가져오는 쿼리문 작성"

--- backend/src/main/java/com/kaspi/backend/dao/GasDetailDao.java
+++ backend/src/main/java/com/kaspi/backend/dao/GasDetailDao.java
@@ -1,10 +1,16 @@
 package com.kaspi.backend.dao;
 
 import com.kaspi.backend.domain.GasDetail;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface GasDetailDao extends CrudRepository<GasDetail, Long> {
-
+    @Value("SELECT * FROM gas_detail where station_no = :stationNo and created_date = :date")
+    Optional<GasDetail> findByStationNoAndDate(@Param("stationNo") Long stationNo, @Param("date") LocalDate date);
 }
