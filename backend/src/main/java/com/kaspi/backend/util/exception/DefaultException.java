be760ed49cf6e6f0e00ec49e479f3784793cdfb6
ajongs <wonjong1030@naver.com>
2023-02-10 19:32:41 +0900

    #100 feat(DefaultException): Code를 사용하도록 수정

--- backend/src/main/java/com/kaspi/backend/util/exception/DefaultException.java
+++ backend/src/main/java/com/kaspi/backend/util/exception/DefaultException.java
@@ -2,23 +2,19 @@ package com.kaspi.backend.util.exception;
 
 import com.kaspi.backend.util.response.code.Code;
 import lombok.Data;
-import org.springframework.http.HttpStatus;
+
 @Data
-public class DefaultException extends RuntimeException{
+public class DefaultException extends RuntimeException {
     private String className;
-    private String errorCode;
-    private String errorMessage;
-    //논의 후 HttpStatus 필드도 추가 예정
-    //private HttpStatus httpStatus;
+    private Code errorCode;
 
     public DefaultException(String className, Code code) {
         this.className = className;
-        this.errorCode = code.getCode();
-        this.errorMessage = code.getMessage();
+        this.errorCode = code;
     }
+
     public DefaultException(Code code) {
         this.className = this.getClass().getSimpleName();
-        this.errorCode = code.getCode();
-        this.errorMessage = code.getMessage();
+        this.errorCode = code;
     }
 }
