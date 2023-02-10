67ba84771cf4e78ed312175d2f71396d52887e00
ajongs <wonjong1030@naver.com>
2023-02-10 19:30:26 +0900

    #100 feat(DefaultCode): 주유소 상세정보를 찾았을 때 반환하는 Code enum 추가

--- backend/src/main/java/com/kaspi/backend/util/response/code/DefaultCode.java
+++ backend/src/main/java/com/kaspi/backend/util/response/code/DefaultCode.java
@@ -4,11 +4,8 @@ public enum DefaultCode implements Code {
 
     SUCCESS_SIGNUP("L001", "정상적으로 회원가입이 완료되었습니다."),
     SUCCESS_VALID_ID("L002", "사용가능한 아이디입니다."),
-    SUCCESS_SIGN_IN("L003", "로그인 성공(세션 생성)");
-
-
-
-
+    SUCCESS_SIGN_IN("L003", "로그인 성공(세션 생성)"),
+    SUCCESS_TO_FIND_GAS_DEATIL("G001", "주유소 가격 상세정보를 찾았습니다.");
     private String code;
     private String message;
 
@@ -16,7 +13,6 @@ public enum DefaultCode implements Code {
         this.code = code;
         this.message = message;
     }
-
 
     @Override
     public String getCode() {
