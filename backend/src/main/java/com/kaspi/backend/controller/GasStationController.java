5c9cfac1064e52218e5dfcca1d943f41db11de2c
ajongs <wonjong1030@naver.com>
2023-02-10 20:29:10 +0900

    #100 feat(GasStationController): 주유소 현재정보를 반환하는 컨트롤러 추가

--- backend/src/main/java/com/kaspi/backend/controller/GasStationController.java
+++ backend/src/main/java/com/kaspi/backend/controller/GasStationController.java
@@ -3,9 +3,14 @@ package com.kaspi.backend.controller;
 import com.kaspi.backend.domain.GasDetailDto;
 import com.kaspi.backend.domain.GasStationDto;
 import com.kaspi.backend.enums.GasType;
+import com.kaspi.backend.service.GasStationService;
+import com.kaspi.backend.util.response.CommonResponseDto;
+import com.kaspi.backend.util.response.code.DefaultCode;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +19,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/gasStation")
+@RequestMapping("api/v1/gas-station")
+@RequiredArgsConstructor
 public class GasStationController {
-    @GetMapping
-    public ResponseEntity<GasStationDto> getGasInfos() {
-        GasDetailDto gasDetailDto = new GasDetailDto(GasType.GASOLINE, 2000, LocalDate.now());
-        List<GasDetailDto> list = new ArrayList<>();
-        list.add(gasDetailDto);
-        GasStationDto gasStationDto = new GasStationDto( "서울 종로구", "㈜지에스이앤알 평창주유소",
-                "서울 종로구 평창문화로 135 (평창동)", "현대오일뱅크", true, list);
-        return new ResponseEntity<>(gasStationDto, HttpStatus.OK);
+    private final GasStationService gasStationService;
+    @GetMapping("/{name}/{roadName}/{buildNum}/{brand}")
+    public ResponseEntity<CommonResponseDto> getGasStationInfoNow(@PathVariable("name") String name,
+                                                          @PathVariable("roadName") String roadName,
+                                                          @PathVariable("buildNum") String buildNum,
+                                                          @PathVariable("brand") String brand) {
+        GasStationDto gasStationDto = gasStationService.findGasStationDto(name, roadName, buildNum, brand);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(CommonResponseDto.toResponse(DefaultCode.SUCCESS_TO_FIND_GAS_DEATIL, gasStationDto));
     }
+
+
+
 }
