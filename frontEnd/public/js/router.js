87e24f8c6746638fd95a404fdcbdd25b7b6fe211
moon-GD <king4mun@gmail.com>
2023-02-10 11:56:49 +0900

    add:#34: 지도 뷰 추가 및 상세 페이지 연결

--- frontEnd/public/js/router.js
+++ frontEnd/public/js/router.js
@@ -6,15 +6,13 @@ import { chartView } from "./myPage/chart/view.js";
 import { comparisonView } from "./myPage/comparison/view.js";
 import { inputOilInfoView } from "./myPage/inputOilInfo/view.js";
 import { historyView } from "./myPage/history/view.js";
+import { gasSTDView } from "./GSTdetail/view.js";
 import { notFoundView } from "./notFound/view.js";
-import { mapView } from "./mapView/view.js";
-import { HEADER, METHOD } from "../common/variable.js";
 
 const $body = document.querySelector("body");
-
 const router = async () => {
     const routes = [
-        { path: "/", view: mainView }, 
+        { path: "/", view: mainView },
         { path: "/register", view: registerView },
         { path: "/login", view: loginView },
         { path: "/userDetail", view: userDetailView },
@@ -22,37 +20,32 @@ const router = async () => {
         { path: "/chart", view: chartView },
         { path: "/comparison", view: comparisonView },
         { path: "/history", view: historyView },
-        { path: "/mapView", view: mapView },
+        { path: "/gasSTDView", view: gasSTDView},
         { path: "/404", view: notFoundView }
     ];
-
     let match = routes.map(route => {
         return {
             route,
             isMatch: location.pathname === route.path
-        };  
+        };
     }).find((routeObj) => routeObj.isMatch)
-
     if(!match) {
         match = {
             route: routes[routes.length - 1],
             isMatch: true
         }
     }
-
     const getNode = match.route.view;
-    $body.replaceWith(getNode());
+    const $container = getNode();
+    $body.replaceWith($container);
 }
-
 // 페이지 전환 함수
 const navigateTo = url => {
     history.pushState(null, null, url);
     router();
 }
-
 window.addEventListener("popstate", () => router());
-window.addEventListener("DOMContentLoaded", () => router());
-
+// window.addEventListener("DOMContentLoaded", () => router());
 // DOM이 렌더링 되면 router 함수 실행
 document.addEventListener("DOMContentLoaded", () => {
     document.body.addEventListener("click", (event) => {
@@ -61,20 +54,5 @@ document.addEventListener("DOMContentLoaded", () => {
             navigateTo((event.target.href));
         }
     })
-
     router();
 });
-
-fetch("http://43.200.157.18:8080/api/v1/user", {
-    method: METHOD.POST,
-    headers: HEADER.POST,
-    body: JSON.stringify({
-        "id":"test3",
-        "password":"testpassword",
-        "gender":"M",
-        "age": "20대"
-    }),
-    withCredentials: true
-}).then((res) => {
-    console.log(res);
-})
