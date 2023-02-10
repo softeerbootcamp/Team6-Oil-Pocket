451b1d9ee90b212a4da4ee520cee7ebe5a879507
rentalhub-mac78 <kksy2423@naver.com>
2023-02-10 19:45:33 +0900

    #110 feat(enum): 오피넷 유종 key로부터 GasType찾는 로직

--- backend/src/main/java/com/kaspi/backend/enums/GasType.java
+++ backend/src/main/java/com/kaspi/backend/enums/GasType.java
@@ -1,8 +1,26 @@
 package com.kaspi.backend.enums;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
 public enum GasType {
-    PREMIUM_GASOLINE,
-    GASOLINE,
-    DIESEL,
-    LPG;
+    PREMIUM_GASOLINE("고급휘발유"),
+    GASOLINE("휘발유"),
+    DIESEL("자동차용경유"),
+    LPG("자동차용부탄");
+
+    private String opinetGasType;
+
+    GasType(String opinetGasType) {
+        this.opinetGasType = opinetGasType;
+    }
+
+    public static GasType getTypeFromOpinetData(String opinetdata) {
+        return Arrays.stream(values())
+                .filter(value -> value.opinetGasType.equals(opinetdata))
+                .findAny().get();
+    }
+
 }
