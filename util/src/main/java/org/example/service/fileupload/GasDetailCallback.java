d6c41ac1ecc441a4075b16bb6aa66a2e6a2545cf
ajongs <wonjong1030@naver.com>
2023-02-11 13:19:30 +0900

    #100 chore(FileUpload): FileUploadService와 관련된 파일을 fileupload 패키지 밑으로 이동

--- util/src/main/java/org/example/service/fileupload/GasDetailCallback.java
+++ util/src/main/java/org/example/service/fileupload/GasDetailCallback.java
@@ -1,4 +1,4 @@
-package org.example.service;
+package org.example.service.fileupload;
 
 
 import org.example.domain.GasDetail;
--- util/src/main/java/org/example/service/fileupload/LpgDetailCallback.java
+++ util/src/main/java/org/example/service/fileupload/LpgDetailCallback.java
@@ -1,7 +1,8 @@
-package org.example.service;
+package org.example.service.fileupload;
 
 import org.example.domain.GasDetail;
 import org.example.domain.GasStation;
+import org.example.service.fileupload.GasDetailCallback;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
