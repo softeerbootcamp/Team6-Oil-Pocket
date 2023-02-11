af2de6cca135e7c0b2f2f90c475bb1a0df2310f9
gyuturn <rbtjrrbals@gmail.com>
2023-02-12 03:31:20 +0900

    #110 feat(service): 유저 주유기록 저장 로직

--- backend/src/main/java/com/kaspi/backend/service/UserRecordService.java
+++ backend/src/main/java/com/kaspi/backend/service/UserRecordService.java
@@ -1,22 +1,31 @@
 package com.kaspi.backend.service;
 
 import com.kaspi.backend.dao.GasDetailDao;
+import com.kaspi.backend.dao.UserGasRecordDao;
 import com.kaspi.backend.domain.GasDetail;
 import com.kaspi.backend.domain.GasStation;
+import com.kaspi.backend.domain.User;
+import com.kaspi.backend.domain.UserGasRecord;
 import com.kaspi.backend.dto.UserGasRecordReqDto;
 import com.kaspi.backend.util.response.code.ErrorCode;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class UserRecordService {
     private final GasDetailDao gasDetailDao;
+    private final HttpSessionService httpSessionService;
+    private final UserGasRecordDao userGasRecordDao;
+
 
 
     /**
@@ -26,7 +35,8 @@ public class UserRecordService {
     public Long calTodayUserGasAmount(UserGasRecordReqDto userGasRecordReqDto, GasStation gasStation) {
         Optional<Long> todayGasPrice = gasDetailDao.findTodayGasPrice(gasStation.getStationNo(),
                 userGasRecordReqDto.getGasType().name(),
-                GasDetail.getNowDateToStr());//오늘 날짜로 계산
+                "2023-02-11");
+//                GasDetail.getNowDateToStr());//오늘 날짜로 계산
         if (todayGasPrice.isEmpty()) {
             log.error("DB에 오늘날짜에 해당되는 주유 가격 정보가 존재하지 않음 가스타입:{}, 주유소PK:{}",userGasRecordReqDto.getGasType().name(),gasStation.getStationNo());
             throw new NoSuchElementException(ErrorCode.SQL_NOT_FOUND.getMessage());
@@ -38,5 +48,21 @@ public class UserRecordService {
 
     public Long calUserSavingAmount(Long userRefuelingPrice, Long userGasAmount, Long nationalAvgOilPrice) {
             return userRefuelingPrice - nationalAvgOilPrice * userGasAmount;
+    }
+
+
+    @Transactional
+    public UserGasRecord saveUserGasRecord(UserGasRecordReqDto userGasRecordReqDto, GasStation gasStation, Long userGasAmount, Long usersSavingPrice) {
+        User user = httpSessionService.getUserFromSession();
+        UserGasRecord userGasRecord = UserGasRecord.builder().userNo(user.getUserNo())
+                .gasStationNo(gasStation.getStationNo())
+                .chargeDate(new Date())
+                .refuelingPrice(userGasRecordReqDto.getRefuelingPrice())
+                .recordGasAmount(userGasAmount) //주유소 주유량
+                .savingPrice(usersSavingPrice) // 사용자 전국 유가 평균값  가져오기
+                .recordGasType(userGasRecordReqDto.getGasType()) // 여기서 변환할때 영어로 에러뜰려나?
+                .build();
+        log.info("유저 주유기록 저장 유저No:{}",user.getUserNo());
+        return userGasRecordDao.save(userGasRecord);
     }
 }
