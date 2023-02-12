3300a5781634f6f8fa7882cdba470ec828dcd801
ajongs <wonjong1030@naver.com>
2023-02-13 00:37:33 +0900

    #129 feat(UserGasRecordDao): 현재 날짜가 포함되는 달의 유저의 gas_record를 반환하는 sql 추가

--- backend/src/main/java/com/kaspi/backend/dao/UserGasRecordDao.java
+++ backend/src/main/java/com/kaspi/backend/dao/UserGasRecordDao.java
@@ -1,10 +1,18 @@
 package com.kaspi.backend.dao;
 
-import com.kaspi.backend.domain.User;
 import com.kaspi.backend.domain.UserGasRecord;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface UserGasRecordDao extends CrudRepository<UserGasRecord, Long> {}
+public interface UserGasRecordDao extends CrudRepository<UserGasRecord, Long> {
+    @Query("select * from user_gas_record where user_no = :userNo and month(charge_date) = month(:date) and year(charge_date) = year(:date)")
+    Optional<List<UserGasRecord>> findByMonthOfNow(@Param("user_no") Long userNo, @Param("date") LocalDate date);
+}
 
