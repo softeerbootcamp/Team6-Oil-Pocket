6272bdb30349367b651e2a64b41c6cea9b56faeb
ajongs <wonjong1030@naver.com>
2023-02-13 00:36:06 +0900

    #129 test(data.sql): 테스트 시 사용할 admin 계정과 user_gas_record 더미데이터 추가

--- backend/src/test/resources/data.sql
+++ backend/src/test/resources/data.sql
@@ -84,3 +84,9 @@ insert into gas_detail (station_no, gas_type, price, created_date)
 values (1, "GASOLINE", 1659, '2023-01-02');
 insert into gas_detail (station_no, gas_type, price, created_date)
 values (1, "DIESEL", 1759, '2023-01-02');
+
+insert into users(id, password, gender, age) values ("admin", "password", "MALE", "FORTHY");
+insert into user_gas_record(user_no, gas_station_no, charge_date, refueling_price, saving_price, record_gas_type, record_gas_amount)
+values (1, 1, "2023-02-12", 23000, 376, "DIESEL", 14);
+insert into user_gas_record(user_no, gas_station_no, charge_date, refueling_price, saving_price, record_gas_type, record_gas_amount)
+values (1, 1, "2023-02-12", 23000, 390, "DIESEL", 14);
