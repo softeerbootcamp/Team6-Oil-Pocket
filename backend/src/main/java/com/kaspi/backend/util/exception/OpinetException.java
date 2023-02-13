bb37e9844ae15f4ed84909aa61befe41ec14d986
rentalhub-mac78 <kksy2423@naver.com>
2023-02-13 11:10:49 +0900

    #110 134 fix:(*): custom expcetion DefaultException상속받도록 구성

--- backend/src/main/java/com/kaspi/backend/util/exception/OpinetException.java
+++ backend/src/main/java/com/kaspi/backend/util/exception/OpinetException.java
@@ -3,9 +3,14 @@ package com.kaspi.backend.util.exception;
 import com.kaspi.backend.util.response.code.Code;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
-@Getter
-@RequiredArgsConstructor
-public class OpinetException extends RuntimeException {
-    private final Code code;
+public class OpinetException extends DefaultException {
+    public OpinetException(String className, Code code) {
+        super(className, code, HttpStatus.GATEWAY_TIMEOUT);
+    }
+
+    public OpinetException(Code code) {
+        super(code,HttpStatus.GATEWAY_TIMEOUT);
+    }
 }
--- backend/src/main/java/com/kaspi/backend/util/exception/SqlNotFoundException.java
+++ backend/src/main/java/com/kaspi/backend/util/exception/SqlNotFoundException.java
@@ -1,13 +1,14 @@
 package com.kaspi.backend.util.exception;
 
 import com.kaspi.backend.util.response.code.Code;
+import org.springframework.http.HttpStatus;
 
 public class SqlNotFoundException extends DefaultException{
     public SqlNotFoundException(String className, Code code) {
-        super(className, code);
+        super(className, code, HttpStatus.NOT_FOUND);
     }
 
     public SqlNotFoundException(Code code) {
-        super(code);
+        super(code,HttpStatus.NOT_FOUND);
     }
 }
