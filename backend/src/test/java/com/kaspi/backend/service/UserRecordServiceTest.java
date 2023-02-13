c4d9f0f09d4bbf6c9f098df87e3bb58fde180840
ajongs <wonjong1030@naver.com>
2023-02-14 00:04:27 +0900

    #129 test(UserGasRecordServiceTest): calMonthUserEcoPrice()메소드의 UserEcoRecordResDto 반환 성공 테스트 추가

--- backend/src/test/java/com/kaspi/backend/service/UserRecordServiceTest.java
+++ backend/src/test/java/com/kaspi/backend/service/UserRecordServiceTest.java
@@ -12,15 +12,16 @@ import com.kaspi.backend.dao.FoodImageDao;
 import com.kaspi.backend.dao.GasDetailDao;
 import com.kaspi.backend.dao.UserGasRecordDao;
 import com.kaspi.backend.domain.*;
-import com.kaspi.backend.domain.GasStation.GasStationBuilder;
 import com.kaspi.backend.dto.UserEcoRecordResDto;
 import com.kaspi.backend.dto.UserGasRecordReqDto;
+import com.kaspi.backend.enums.Age;
 import com.kaspi.backend.enums.GasType;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.*;
 
+import com.kaspi.backend.enums.Gender;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
@@ -203,8 +204,40 @@ class UserRecordServiceTest {
 
         SoftAssertions softly = new SoftAssertions();
         softly.assertThat(userEcoRecordResDto.getRefuelingPrice()).isEqualTo(50000);
-        softly.assertThat(userEcoRecordResDto.getEcoPrice()).isEqualTo(4000);
+        softly.assertThat(userEcoRecordResDto.getMyEcoPrice()).isEqualTo(4000);
         softly.assertThat(userEcoRecordResDto.getImageUrl()).isEqualTo("https://team6-public-image.s3.ap-northeast-2.amazonaws.com/food/coffee.png");
+        softly.assertAll();
+    }
+    @Test
+    @DisplayName("calMonthUserEcoPrice()메소드의 UserEcoRecordResDto 반환 성공 테스트")
+    public void calMonthUserEcoPrice_ShouldReturnUserEcoRecordResDto() {
+        User user = User.builder().id("test_id").userNo(1L).gender(Gender.MALE).age(Age.FORTY).build();
+        UserGasRecord userGasRecord1 = UserGasRecord.builder().refuelingPrice(10000L).savingPrice(5000L).build();
+        UserGasRecord userGasRecord2 = UserGasRecord.builder().refuelingPrice(20000L).savingPrice(6000L).build();
+        List<UserGasRecord> userGasRecords = Arrays.asList(userGasRecord1, userGasRecord2);
+        FoodImage foodImage = FoodImage.builder().imageUrl("http://food.com").build();
+        EcoRecord ecoRecord1 = EcoRecord.builder().userNo(1L).saving_price(3000).build();
+        EcoRecord ecoRecord2 = EcoRecord.builder().userNo(2L).saving_price(2000).build();
+        EcoRecord ecoRecord3 = EcoRecord.builder().userNo(3L).saving_price(1000).build();
+        List<EcoRecord> ecoRecords = Arrays.asList(ecoRecord1, ecoRecord2, ecoRecord3);
+
+        when(httpSessionService.getUserFromSession()).thenReturn(user);
+        when(userGasRecordDao.findByMonthOfNow(user.getUserNo(), LocalDate.now())).thenReturn(Optional.of(userGasRecords));
+        when(foodImageDao.findFoodImageByEcoPrice(BigDecimal.valueOf(11000))).thenReturn(Optional.of(foodImage));
+        when(userGasRecordDao.findSavingPriceByGenderAndAge(user.getGender(), user.getAge())).thenReturn(Optional.of(ecoRecords));
+
+        UserEcoRecordResDto actual = userRecordService.calMonthUserEcoPrice();
+
+        SoftAssertions softly = new SoftAssertions();
+
+        softly.assertThat(actual.getUserId()).isEqualTo("test_id");
+        softly.assertThat(actual.getGender()).isEqualTo(Gender.MALE);
+        softly.assertThat(actual.getAge()).isEqualTo(Age.FORTY);
+        softly.assertThat(actual.getRefuelingPrice()).isEqualTo(30000L);
+        softly.assertThat(actual.getMyEcoPrice()).isEqualTo(11000L);
+        softly.assertThat(actual.getAverageEcoPrice()).isEqualTo(2000L);
+        softly.assertThat(actual.getImageUrl()).isEqualTo("http://food.com");
+        softly.assertThat(actual.getRankPercentage()).isEqualTo(33.33);
         softly.assertAll();
     }
 }
