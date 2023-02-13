0f3863e92cdbc73611fb33c61daeba9d2231e81d
ajongs <wonjong1030@naver.com>
2023-02-13 23:58:35 +0900

    #129 feat(UserEcoRecordResDto): UserEcoRecordResDto 추가

--- backend/src/main/java/com/kaspi/backend/dto/UserEcoRecordResDto.java
+++ backend/src/main/java/com/kaspi/backend/dto/UserEcoRecordResDto.java
@@ -3,17 +3,21 @@ package com.kaspi.backend.dto;
 import com.kaspi.backend.dao.FoodImageDao;
 import com.kaspi.backend.domain.FoodImage;
 import com.kaspi.backend.domain.UserGasRecord;
+import com.kaspi.backend.enums.Age;
+import com.kaspi.backend.enums.Gender;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class UserEcoRecordResDto {
+    private String userId;
+    private Gender gender;
+    private Age age;
     private long refuelingPrice;
-    private long ecoPrice;
+    private long myEcoPrice;
+    private long averageEcoPrice;
     private String imageUrl;
+    private double rankPercentage;
 
-    public static UserEcoRecordResDto newInstance(long refuelingPrice, long ecoPrice, FoodImage foodImage) {
-        return new UserEcoRecordResDto(refuelingPrice, ecoPrice, foodImage.getImageUrl());
-    }
 }
