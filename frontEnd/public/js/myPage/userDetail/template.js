3882d431ff2d2bdcc6515527b9c5c67b81014d6d
moon-GD <king4mun@gmail.com>
2023-02-14 23:08:24 +0900

    modify:dev(마이페이지 프로필 수정 로그인 정보와 연동)

--- frontEnd/public/js/myPage/userDetail/template.js
+++ frontEnd/public/js/myPage/userDetail/template.js
@@ -1,3 +1,5 @@
+import { userId, userAge, userGender } from "../../login/fetch";
+
 const getUserDetailTemplate = () => `
     <section class="oilInfoArea">
         <section class="oilInfoArea__background">
@@ -12,30 +14,30 @@ const getUserDetailTemplate = () => `
                 <div class="oilInfoArea__profileContainer">
                     <div class="oilInfoArea__profileTitleBox">
                         <h2>주행자</h2>
-                        <span>jaewon</span>
+                        <span>${userId ? userId : ""}</span>
                     </div>
                     <div class="oilInfoArea__profileContentBox">
                         <div class="oilInfoArea__profileContentBox--gender">
                             <h2>성별</h2>
                             <div class="oilInfoArea__inputBoxArea--gender">
-                                <input type="radio" name="gender" value="남자" id="radio--male" checked>
+                                <input type="radio" name="gender" value="남자" id="radio--male" ${userGender === "M" ? "checked" : ""}>
                                 <label for="radio--male">남자</label>
-                                <input type="radio" name="gender" value="여자" id="radio--female">
+                                <input type="radio" name="gender" value="여자" id="radio--female" ${userGender === "F" ? "checked" : ""}>
                                 <label for="radio--female">여자</label>
                             </div>
                         </div>
                         <div class="oilInfoArea__profileContentBox--age">
                             <h2>나이</h2>
                             <div class="oilInfoArea__inputBoxArea--age">
-                                <input type="radio" name="age" value="20대" id="radioAge--20" checked>
+                                <input type="radio" name="age" value="20대" id="radioAge--20" ${userAge === "20대" ? "checked" : ""}>
                                 <label for="radioAge--20">20대</label>
-                                <input type="radio" name="age" value="30대" id="radioAge--30">
+                                <input type="radio" name="age" value="30대" id="radioAge--30" ${userAge === "30대" ? "checked" : ""}>
                                 <label for="radioAge--30">30대</label>
-                                <input type="radio" name="age" value="40대" id="radioAge--40">
+                                <input type="radio" name="age" value="40대" id="radioAge--40" ${userAge === "40대" ? "checked" : ""}>
                                 <label for="radioAge--40">40대</label>
-                                <input type="radio" name="age" value="50대" id="radioAge--50">
+                                <input type="radio" name="age" value="50대" id="radioAge--50" ${userAge === "50대" ? "checked" : ""}>
                                 <label for="radioAge--50">50대</label>
-                                <input type="radio" name="age" value="60대 이상" id="radioAge--60">
+                                <input type="radio" name="age" value="60대 이상" id="radioAge--60" ${userAge === "60대 이상" ? "checked" : ""}>
                                 <label for="radioAge--60">60대 이상</label>
                             </div>
                         </div>
