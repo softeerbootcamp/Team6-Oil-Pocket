fd72bcbf7358157bf68d4277099201f71143742f
rentalhub-mac78 <kksy2423@naver.com>
2023-02-14 18:13:14 +0900

    #133: feat(dao): 특정 사용자에 맞는 UserGasRecord 가져오기

--- backend/src/main/java/com/kaspi/backend/dao/UserGasRecordDao.java
+++ backend/src/main/java/com/kaspi/backend/dao/UserGasRecordDao.java
@@ -2,9 +2,19 @@ package com.kaspi.backend.dao;
 
 import com.kaspi.backend.domain.User;
 import com.kaspi.backend.domain.UserGasRecord;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface UserGasRecordDao extends CrudRepository<UserGasRecord, Long> {}
+public interface UserGasRecordDao extends CrudRepository<UserGasRecord, Long> {
+    @Query("select charge_date,gas_station_no, record_gas_type ,record_gas_amount ,refueling_price ,saving_price  from user_gas_record ugr " +
+            "where user_no = :userNo;")
+    List<UserGasRecord> findGasRecordListByUserId(@Param("userNo") Long userNo);
+}
 
