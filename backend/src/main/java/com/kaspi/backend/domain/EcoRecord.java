f66a998e355068b77ff0fe1705aae62d94498df6
ajongs <wonjong1030@naver.com>
2023-02-14 12:06:24 +0900

    #129 feat(EcoRecord): 주유 소비 가격, 속한 그룹의 percent_rank를 담는 멤버변수 추가

--- backend/src/main/java/com/kaspi/backend/domain/EcoRecord.java
+++ backend/src/main/java/com/kaspi/backend/domain/EcoRecord.java
@@ -2,19 +2,23 @@ package com.kaspi.backend.domain;
 
 import com.kaspi.backend.enums.Age;
 import com.kaspi.backend.enums.Gender;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class EcoRecord {
     private long userNo;
     private Gender gender;
     private Age age;
-    private long saving_price;
+    private long refuelingPrice;
+    private long savingPrice;
+    private double perRank;
 
     @Override
     public boolean equals(Object obj) {
-        return this.userNo == ((EcoRecord)obj).userNo;
+        return this.userNo == ((EcoRecord) obj).userNo;
     }
 }
