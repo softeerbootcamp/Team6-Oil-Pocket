a14a13adf3dd80d510bc591991b31d774d4781a9
rentalhub-mac78 <kksy2423@naver.com>
2023-02-14 18:31:53 +0900

    #133: test(dto): userGasRecord를 response dto로 변환하는 로직 변경

---
+++ backend/src/test/java/com/kaspi/backend/dto/UserGasRecordResDtoTest.java
@@ -0,0 +1,42 @@
+package com.kaspi.backend.dto;
+
+import com.kaspi.backend.domain.GasStation;
+import com.kaspi.backend.domain.UserGasRecord;
+import com.kaspi.backend.enums.GasBrand;
+import com.kaspi.backend.enums.GasType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserGasRecordResDtoTest {
+
+    @Test
+    @DisplayName("사용자 주유 기록 dto로 변환하는 로직 테스트")
+    void toUserGasRecordResDto()  {
+        //given
+        UserGasRecord userGasRecord = UserGasRecord.builder()
+                .chargeDate(new Date())
+                .recordGasType(GasType.GASOLINE)
+                .recordGasAmount(12L)
+                .refuelingPrice(50000L)
+                .savingPrice(120L).build();
+
+        GasStation gasStation = GasStation.builder()
+                .name("Test Gas Station")
+                .brand(GasBrand.SK_GAS.getDbName()).build();
+
+        //when
+        UserGasRecordResDto dto = UserGasRecordResDto.toUserGasRecordResDto(userGasRecord, gasStation);
+        //then
+        assertEquals(userGasRecord.getRecordGasType().getOpinetGasType(), dto.getGasType());
+        assertEquals(userGasRecord.getRecordGasAmount() + "L", dto.getRecordGasAmount());
+        assertEquals(userGasRecord.getRefuelingPrice() + "원", dto.getRefuelingPrice());
+        assertEquals(userGasRecord.getSavingPrice() + "원", dto.getSavingPrice());
+        assertEquals(GasBrand.getImg(gasStation.getBrand()), dto.getBrand());
+        assertEquals(gasStation.getName(), dto.getGasStationName());
+        assertEquals(userGasRecord.getChargeDate().toString().replace("-", "."), dto.getChargeDate());
+    }
+}
