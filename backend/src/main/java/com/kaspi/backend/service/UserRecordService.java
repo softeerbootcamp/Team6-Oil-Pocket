582f71af9f170f5f6606aad5476f3be049c8dc92
gyuturn <rbtjrrbals@gmail.com>
2023-02-15 11:44:07 +0900

    #133 feat(service): 사용자의 주유기록 입력을 가저오는 비지니스로직

--- backend/src/main/java/com/kaspi/backend/service/UserRecordService.java
+++ backend/src/main/java/com/kaspi/backend/service/UserRecordService.java
@@ -12,6 +12,7 @@ import com.kaspi.backend.dto.UserGasRecordResDto;
 import com.kaspi.backend.util.exception.SqlNotFoundException;
 import com.kaspi.backend.util.response.code.ErrorCode;
 
+import java.time.LocalDate;
 import java.util.*;
 
 import lombok.RequiredArgsConstructor;
@@ -29,9 +30,7 @@ public class UserRecordService {
     private final UserGasRecordDao userGasRecordDao;
 
 
-
     /**
-     *
      * 사용자로 부터 받은 주유금액, 유종과 주유소 정보로 얼마만큼 넣었는지 계산하는 로직
      */
     public Long calTodayUserGasAmount(UserGasRecordReqDto userGasRecordReqDto, GasStation gasStation) {
@@ -39,36 +38,38 @@ public class UserRecordService {
                 userGasRecordReqDto.getGasType().name(),
                 GasDetail.getNowDateToStr());//오늘 날짜로 계산
         if (todayGasPrice.isEmpty()) {
-            log.error("DB에 오늘날짜에 해당되는 주유 가격 정보가 존재하지 않음 가스타입:{}, 주유소PK:{}",userGasRecordReqDto.getGasType().name(),gasStation.getStationNo());
+            log.error("DB에 오늘날짜에 해당되는 주유 가격 정보가 존재하지 않음 가스타입:{}, 주유소PK:{}", userGasRecordReqDto.getGasType().name(),
+                    gasStation.getStationNo());
             throw new NoSuchElementException(ErrorCode.SQL_NOT_FOUND.getMessage());
         }
         Long userGasAmount = (long) Math.round(userGasRecordReqDto.getRefuelingPrice() / todayGasPrice.get());
-        log.info("사용자가 주유한 가스타입:{}, 주유량:{}",userGasRecordReqDto.getGasType().name(),userGasAmount);
+        log.info("사용자가 주유한 가스타입:{}, 주유량:{}", userGasRecordReqDto.getGasType().name(), userGasAmount);
         return userGasAmount;
     }
 
     public Long calUserSavingAmount(Long userRefuelingPrice, Long userGasAmount, Long nationalAvgOilPrice) {
-            return userRefuelingPrice - nationalAvgOilPrice * userGasAmount;
+        return userRefuelingPrice - nationalAvgOilPrice * userGasAmount;
     }
 
 
     @Transactional
-    public UserGasRecord saveUserGasRecord(UserGasRecordReqDto userGasRecordReqDto, GasStation gasStation, Long userGasAmount, Long usersSavingPrice) {
+    public UserGasRecord saveUserGasRecord(UserGasRecordReqDto userGasRecordReqDto, GasStation gasStation,
+                                           Long userGasAmount, Long usersSavingPrice) {
         User user = httpSessionService.getUserFromSession();
         UserGasRecord userGasRecord = UserGasRecord.builder().userNo(user.getUserNo())
                 .gasStationNo(gasStation.getStationNo())
-                .chargeDate(new Date())
+                .chargeDate(LocalDate.now())
                 .refuelingPrice(userGasRecordReqDto.getRefuelingPrice())
                 .recordGasAmount(userGasAmount) //주유소 주유량
                 .savingPrice(usersSavingPrice) // 사용자 전국 유가 평균값  가져오기
                 .recordGasType(userGasRecordReqDto.getGasType()) // 여기서 변환할때 영어로 에러뜰려나?
                 .build();
-        log.info("유저 주유기록 저장 유저No:{}",user.getUserNo());
+        log.info("유저 주유기록 저장 유저No:{}", user.getUserNo());
         return userGasRecordDao.save(userGasRecord);
     }
 
     /**
-     *유저가 저장한 주유기록을 가져오는 로직
+     * 유저가 저장한 주유기록을 가져오는 로직
      */
     public List<UserGasRecordResDto> getUserRecords() {
         User user = httpSessionService.getUserFromSession();
@@ -84,7 +85,6 @@ public class UserRecordService {
 
         for (UserGasRecord userGasRecord : matchingUserGasRecords) {
             GasStation findGasStation = getGasStation(userGasRecord);
-
             //유저 주유 기록과 주유할때 입력한 주유소를 통해 주유기록 열람 dto를 생성
             userRecordsRes.add(UserGasRecordResDto.toUserGasRecordResDto(userGasRecord, findGasStation));
         }
@@ -93,8 +93,8 @@ public class UserRecordService {
 
     private GasStation getGasStation(UserGasRecord userGasRecord) {
         Optional<GasStation> gasStation = gasStationDao.findById(userGasRecord.getGasStationNo());
-        if(gasStation.isEmpty()){
-            log.error("GasStation을 가져오는 쿼리에서 해당 GasStation이 없습니다. gasStationNo:{}",userGasRecord.getGasStationNo());
+        if (gasStation.isEmpty()) {
+            log.error("GasStation을 가져오는 쿼리에서 해당 GasStation이 없습니다. gasStationNo:{}", userGasRecord.getGasStationNo());
             throw new SqlNotFoundException(ErrorCode.SQL_NOT_FOUND);
         }
         return gasStation.get();
