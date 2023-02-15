05db7478c0ad7fa81877e104b132222e02beb1b0
ajongs <wonjong1030@naver.com>
2023-02-15 21:07:58 +0900

    #171 feat(GasStationDao): 모든 gasStation 레코드를 가져오는 쿼리 추가

--- util/src/main/java/org/example/dao/GasStationDao.java
+++ util/src/main/java/org/example/dao/GasStationDao.java
@@ -6,10 +6,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface GasStationDao extends CrudRepository<GasStation, Long> {
 
     @Query("select * from gas_station where address like :address and brand = :brand")
     public Optional<GasStation> findByAddressAndBrand(@Param("address") String address, @Param("brand") String brand);
+
+    @Query("select * from gas_station")
+    Optional<List<GasStation>> findAllGasStation();
 }
