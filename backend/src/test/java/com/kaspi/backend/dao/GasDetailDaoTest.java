0d3c21d1f4867252dda8985a5f1062ab257bd287
ajongs <wonjong1030@naver.com>
2023-02-16 16:55:49 +0900

    #186 test(GasDetailDaoTest): 오름차순 정렬 테스트

--- backend/src/test/java/com/kaspi/backend/dao/GasDetailDaoTest.java
+++ backend/src/test/java/com/kaspi/backend/dao/GasDetailDaoTest.java
@@ -4,6 +4,7 @@ import com.kaspi.backend.domain.GasDetail;
 import com.kaspi.backend.domain.GasStation;
 import com.kaspi.backend.util.config.TestRedisConfiguration;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -14,8 +15,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 
 @DataJdbcTest
 @ContextConfiguration(classes = {TestRedisConfiguration.class})
@@ -77,5 +81,18 @@ class GasDetailDaoTest {
             softAssertions.assertThat(gasDetail.getDate()).isBetween(LocalDate.now().minusMonths(1), LocalDate.now());
         }
         softAssertions.assertAll();
+    }
+    @Test
+    @DisplayName("1달 주유소 정보 날짜 오름차순 정렬 테스트")
+    void findByStationAndOneMonth_asc() {
+        //given
+        LocalDate date = LocalDate.of(2023, 2, 12);
+
+        Optional<GasStation> optionalGasStation = gasStationDao.findByAddressAndBrand("평창문화로 135", "현대오일뱅크");
+        GasStation gasStation = optionalGasStation.get();
+        Optional<List<GasDetail>> optionalGasDetails = gasDetailDao.findByStationAndOneMonth(gasStation.getStationNo(), date);
+        List<GasDetail> gasDetails = optionalGasDetails.get();
+
+        assertThat(gasDetails).isSortedAccordingTo(Comparator.comparing(GasDetail::getDate));
     }
 }
