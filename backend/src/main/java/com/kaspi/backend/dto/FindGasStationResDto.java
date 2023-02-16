603182f48325fcb48f93b685a43cc2c7f02f304d
rentalhub-mac78 <kksy2423@naver.com>
2023-02-16 17:53:58 +0900

    #185 feat&test(dto): 주유소 검색 dto 동등성 비교 커스텀

--- backend/src/main/java/com/kaspi/backend/dto/FindGasStationResDto.java
+++ backend/src/main/java/com/kaspi/backend/dto/FindGasStationResDto.java
@@ -1,14 +1,38 @@
 package com.kaspi.backend.dto;
 
+import com.kaspi.backend.domain.GasStationDto;
+import com.kaspi.backend.enums.GasBrand;
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.io.Serializable;
+import java.util.Objects;
 
 @Builder
 @Getter
-public class FindGasStationResDto {
+@RedisHash("gasStation")
+public class FindGasStationResDto implements Serializable {
     private Long stationNo;
     private String area;
     private String name;
     private String address;
     private String brand;
+
+    public static FindGasStationResDto toFindDto(GasStationDto gasStationDto) {
+        return FindGasStationResDto.builder()
+                .stationNo(gasStationDto.getStationNo())
+                .area(gasStationDto.getArea())
+                .name(gasStationDto.getName())
+                .address(gasStationDto.getAddress())
+                .brand(GasBrand.getImgByDbName(gasStationDto.getBrand()))
+                .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        FindGasStationResDto opposite = (FindGasStationResDto) o;
+        return this.stationNo == opposite.stationNo;
+    }
+
 }
---
+++ backend/src/test/java/com/kaspi/backend/dto/FindGasStationResDtoTest.java
@@ -0,0 +1,33 @@
+package com.kaspi.backend.dto;
+
+import com.kaspi.backend.enums.GasBrand;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindGasStationResDtoTest {
+
+    @Test
+    @DisplayName("주유소 검색 메서드 custom equal")
+    void testEquals() {
+        //given
+        FindGasStationResDto origin = FindGasStationResDto.builder().stationNo(1L).brand(GasBrand.SK_GAS.getDbName()).build();
+        FindGasStationResDto opposite = FindGasStationResDto.builder().stationNo(1L).brand(GasBrand.SK_ENERGY.getDbName()).build();
+        //when
+        //then
+        Assertions.assertThat(origin.equals(opposite)).isTrue();
+    }
+
+    @Test
+    @DisplayName("주유소 검색 메서드 custom equal 실패")
+    void testEquals_Fail() {
+        //given
+        FindGasStationResDto origin = FindGasStationResDto.builder().stationNo(1L).brand(GasBrand.SK_GAS.getDbName()).build();
+        FindGasStationResDto opposite = FindGasStationResDto.builder().stationNo(2L).brand(GasBrand.SK_ENERGY.getDbName()).build();
+        //when
+        //then
+        Assertions.assertThat(origin.equals(opposite)).isFalse();
+    }
+}
