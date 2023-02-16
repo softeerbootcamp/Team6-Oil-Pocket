920fb5b5107a379cd96c27b64815a275dd102950
rentalhub-mac78 <kksy2423@naver.com>
2023-02-16 18:53:25 +0900

    #185 feat(service): 최근본 주유소 세션 저장 로직

--- backend/src/main/java/com/kaspi/backend/service/HttpSessionService.java
+++ backend/src/main/java/com/kaspi/backend/service/HttpSessionService.java
@@ -1,11 +1,15 @@
 package com.kaspi.backend.service;
 
 import com.kaspi.backend.dao.UserDao;
+import com.kaspi.backend.domain.GasStationDto;
 import com.kaspi.backend.domain.User;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import javax.servlet.http.HttpSession;
 
+import com.kaspi.backend.dto.FindGasStationResDto;
 import com.kaspi.backend.util.exception.AuthenticationException;
 import com.kaspi.backend.util.response.code.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -46,6 +50,36 @@ public class HttpSessionService {
 
     public void deleteSession() {
         httpSession.invalidate();
+    }
+
+
+    /**
+     * 사용자가 주유소 상세페이지를 들어갔을 경우 해당 주유소를 세션에 저장
+     * 추후 주유기록 입력시에 최근 본 주유소를 리스트로 보여주기 위함
+     * 로그인 되어 있지 않으면 실행되지 않는 함수
+     */
+    public void addRecentStationView(GasStationDto gasStationDto) {
+        try {
+            checkSessionValid();
+        } catch (AuthenticationException e) {
+            return; //로그인이 되어 있지 않으면 패스
+        }
+        List<FindGasStationResDto> recentGasStation = (List<FindGasStationResDto>) httpSession.getAttribute(SESSION_RECENT_VIEW_STATION_KEY);
+        recentGasStation = updateRecentGasStation(gasStationDto, recentGasStation);
+        httpSession.setAttribute(SESSION_RECENT_VIEW_STATION_KEY, recentGasStation);
+    }
+
+    private List<FindGasStationResDto> updateRecentGasStation(GasStationDto gasStationDto, List<FindGasStationResDto> recentGasStation) {
+        if (recentGasStation == null||recentGasStation.size()==0) {
+            recentGasStation = new ArrayList<>(); //처음 조회한 경우
+        }
+        FindGasStationResDto findGasStationResDto = FindGasStationResDto.toFindDto(gasStationDto);
+        findGasStationResDto.getStationNo();
+        if (!recentGasStation.contains(findGasStationResDto)) {
+            recentGasStation.add(findGasStationResDto); //이전에 조회하지않았다면 session에 저장
+            log.info("사용자가 주유소를 조회했습니다. 세션에 저장됩니다. 최근 본 주유소:{}", gasStationDto.getName());
+        }
+        return recentGasStation;
     }
 
 
