667d0e678018aefb049350958ef13c6493127293
ajongs <wonjong1030@naver.com>
2023-02-20 15:18:28 +0900

    #221 feat(EcoRecord): perRank를 sRank로 수정

--- backend/src/main/java/com/kaspi/backend/domain/EcoRecord.java
+++ backend/src/main/java/com/kaspi/backend/domain/EcoRecord.java
@@ -15,7 +15,7 @@ public class EcoRecord {
     private Age age;
     private long refuelingPrice;
     private long savingPrice;
-    private double perRank;
+    private long sRank;
 
     @Override
     public boolean equals(Object obj) {
