b3e7b470a3001f071a67afe6cdfdbbdcc46d6633
rentalhub-mac78 <kksy2423@naver.com>
2023-02-22 14:06:57 +0900

    #261 fix(*): 주유기록 조회시 gastype dto 한글로 변경

--- backend/src/main/java/com/kaspi/backend/enums/GasType.java
+++ backend/src/main/java/com/kaspi/backend/enums/GasType.java
@@ -7,15 +7,17 @@ import java.util.Optional;
 
 @Getter
 public enum GasType {
-    PREMIUM_GASOLINE("고급휘발유"),
-    GASOLINE("휘발유"),
-    DIESEL("자동차용경유"),
-    LPG("자동차용부탄");
+    PREMIUM_GASOLINE("고급휘발유","고급휘발유"),
+    GASOLINE("휘발유","휘발유"),
+    DIESEL("자동차용경유","경유"),
+    LPG("자동차용부탄","LPG");
 
     private String opinetGasType;
+    private String dtoName;
 
-    GasType(String opinetGasType) {
+    GasType(String opinetGasType,String dtoName) {
         this.opinetGasType = opinetGasType;
+        this.dtoName = dtoName;
     }
 
     public static Optional<GasType> getTypeFromOpinetData(String opinetdata) {
